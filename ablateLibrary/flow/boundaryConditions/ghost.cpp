3509e54948202397467f6e45993ac379c298ae24
matt mcgurn <matt@mcgurn.dev>
2021-08-22 09:30:19 -0400

    added simple zero grad ghost boundary

--- ablateLibrary/flow/boundaryConditions/ghost.cpp
+++ ablateLibrary/flow/boundaryConditions/ghost.cpp
@@ -16,10 +16,11 @@ ablate::flow::boundaryConditions::Ghost::Ghost(std::string fieldName, std::strin
 
 void ablate::flow::boundaryConditions::Ghost::SetupBoundary(PetscDS problem, PetscInt fieldId) {
     PetscDSAddBoundary(
-        problem, DM_BC_NATURAL_RIEMANN, GetBoundaryName().c_str(), labelName.c_str(), fieldId, 0, NULL, (void (*)(void))updateFunction, NULL, labelIds.size(), &labelIds[0], (void *)updateContext) >>
+        problem, DM_BC_NATURAL_RIEMANN, GetBoundaryName().c_str(), labelName.c_str(), fieldId, 0, NULL, (void(*)(void))updateFunction, NULL, labelIds.size(), &labelIds[0], (void *)updateContext) >>
         checkError;
 
     // extract some information about the flowField
     PetscDSGetFieldSize(problem, fieldId, &fieldSize) >> checkError;
     PetscDSGetCoordinateDimension(problem, &dim) >> checkError;
+    PetscDSGetFieldOffset(problem, fieldId, &fieldOffset) >> checkError;
 }
