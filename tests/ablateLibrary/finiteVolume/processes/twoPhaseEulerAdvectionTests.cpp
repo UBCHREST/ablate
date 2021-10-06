18c3f0d831b1dd9e51215d364de0076bcc2afd68
maesemen <maesemen@buffalo.edu>
2021-10-06 14:02:37 -0400

    2 phase advection test decode

--- tests/ablateLibrary/finiteVolume/processes/twoPhaseEulerAdvectionTests.cpp
+++ tests/ablateLibrary/finiteVolume/processes/twoPhaseEulerAdvectionTests.cpp
@@ -44,52 +44,52 @@ struct TwoPhaseEulerAdvectionTestDecodeStateParameters {
 class TwoPhaseEulerAdvectionTestDecodeStateFixture : public testingResources::PetscTestFixture, public ::testing::WithParamInterface<TwoPhaseEulerAdvectionTestDecodeStateParameters> {};
 
 TEST_P(TwoPhaseEulerAdvectionTestDecodeStateFixture, ShouldDecodeState) {
-// arrange
-auto parameters = std::make_shared<ablate::parameters::MapParameters>(GetParam().options);
-//std::shared_ptr<ablate::eos::EOS> eosGas = std::make_shared<ablate::eos::PerfectGas>(parameters);
-//std::shared_ptr<ablate::eos::EOS> eosLiquid = std::make_shared<ablate::eos::PerfectGas>(parameters);
+    // arrange
+    auto parameters = std::make_shared<ablate::parameters::MapParameters>(GetParam().options);
+    //std::shared_ptr<ablate::eos::EOS> eosGas = std::make_shared<ablate::eos::PerfectGas>(parameters);
+    //std::shared_ptr<ablate::eos::EOS> eosLiquid = std::make_shared<ablate::eos::PerfectGas>(parameters);
 
-// get the test params
-const auto& params = GetParam();
+    // get the test params
+    const auto& params = GetParam();
 
-// Prepare outputs
-PetscReal density;
-PetscReal densityG;
-PetscReal densityL;
-PetscReal normalVelocity;
-std::vector<PetscReal> velocity;
-PetscReal internalEnergy;
-PetscReal internalEnergyG;
-PetscReal internalEnergyL;
-PetscReal soundSpeedG;
-PetscReal soundSpeedL;
-PetscReal MG;
-PetscReal ML;
-PetscReal pressure;
-PetscReal alpha;
+    // Prepare outputs
+    PetscReal density;
+    PetscReal densityG;
+    PetscReal densityL;
+    PetscReal normalVelocity;
+    std::vector<PetscReal> velocity;
+    PetscReal internalEnergy;
+    PetscReal internalEnergyG;
+    PetscReal internalEnergyL;
+    PetscReal soundSpeedG;
+    PetscReal soundSpeedL;
+    PetscReal MG;
+    PetscReal ML;
+    PetscReal pressure;
+    PetscReal alpha;
 
-// act
-PetscErrorCode ierr = flow::processes::TwoPhaseEulerAdvection::DecodeTwoPhaseEulerState(
-    params.eosGas, params.eosLiquid, params.dim, &params.conservedValuesIn[0], &params.normalIn[0], &density, &densityG, &densityL, &normalVelocity, &velocity[0], &internalEnergy, &internalEnergyG, &internalEnergyL, &soundSpeedG, &soundSpeedL, &MG, &ML, &pressure, &alpha);
+    // act
+    PetscErrorCode ierr = flow::processes::TwoPhaseEulerAdvection::DecodeTwoPhaseEulerState(
+        params.eosGas, params.eosLiquid, params.dim, &params.conservedValuesIn[0], &params.normalIn[0], &density, &densityG, &densityL, &normalVelocity, &velocity[0], &internalEnergy, &internalEnergyG, &internalEnergyL, &soundSpeedG, &soundSpeedL, &MG, &ML, &pressure, &alpha);
 
-// assert
-ASSERT_EQ(ierr, 0);
-ASSERT_NEAR(density, params.expectedDensity, 1E-6);
-ASSERT_NEAR(densityG, params.expectedDensityG, 1E-6);
-ASSERT_NEAR(densityL, params.expectedDensityL, 1E-6);
-ASSERT_NEAR(normalVelocity, params.expectedNormalVelocity, 1E-6);
-ASSERT_NEAR(velocity[0], params.expectedVelocity[0], 1E-6);
-ASSERT_NEAR(velocity[1], params.expectedVelocity[1], 1E-6);
-ASSERT_NEAR(velocity[2], params.expectedVelocity[2], 1E-6);
-ASSERT_NEAR(internalEnergy, params.expectedInternalEnergy, 1E-6);
-ASSERT_NEAR(internalEnergyG, params.expectedInternalEnergyG, 1E-6);
-ASSERT_NEAR(internalEnergyL, params.expectedInternalEnergyL, 1E-6);
-ASSERT_NEAR(soundSpeedG, params.expectedSoundSpeedG, 1E-6);
-ASSERT_NEAR(soundSpeedL, params.expectedSoundSpeedL, 1E-6);
-ASSERT_NEAR(MG, params.expectedMG, 1E-6);
-ASSERT_NEAR(ML, params.expectedML, 1E-6);
-ASSERT_NEAR(pressure, params.expectedPressure, 1E-6);
-ASSERT_NEAR(alpha, params.expectedAlpha, 1E-6);
+    // assert
+    ASSERT_EQ(ierr, 0);
+    ASSERT_NEAR(density, params.expectedDensity, 1E-6);
+    ASSERT_NEAR(densityG, params.expectedDensityG, 1E-6);
+    ASSERT_NEAR(densityL, params.expectedDensityL, 1E-6);
+    ASSERT_NEAR(normalVelocity, params.expectedNormalVelocity, 1E-6);
+    ASSERT_NEAR(velocity[0], params.expectedVelocity[0], 1E-6);
+    ASSERT_NEAR(velocity[1], params.expectedVelocity[1], 1E-6);
+    ASSERT_NEAR(velocity[2], params.expectedVelocity[2], 1E-6);
+    ASSERT_NEAR(internalEnergy, params.expectedInternalEnergy, 1E-6);
+    ASSERT_NEAR(internalEnergyG, params.expectedInternalEnergyG, 1E-6);
+    ASSERT_NEAR(internalEnergyL, params.expectedInternalEnergyL, 1E-6);
+    ASSERT_NEAR(soundSpeedG, params.expectedSoundSpeedG, 1E-6);
+    ASSERT_NEAR(soundSpeedL, params.expectedSoundSpeedL, 1E-6);
+    ASSERT_NEAR(MG, params.expectedMG, 1E-6);
+    ASSERT_NEAR(ML, params.expectedML, 1E-6);
+    ASSERT_NEAR(pressure, params.expectedPressure, 1E-6);
+    ASSERT_NEAR(alpha, params.expectedAlpha, 1E-6);
 }
 
 INSTANTIATE_TEST_SUITE_P(TwoPhaseEulerAdvectionTests, TwoPhaseEulerAdvectionTestDecodeStateFixture,
@@ -127,4 +127,4 @@ INSTANTIATE_TEST_SUITE_P(TwoPhaseEulerAdvectionTests, TwoPhaseEulerAdvectionTest
                         .expectedML = 1.2,
                         .expectedPressure = 47664,
                         .expectedAlpha = 0.5}),
-[](const testing::TestParamInfo<TwoPhaseEulerAdvectionTestDecodeStateParameters>& info) { return std::to_string(info.index); });
+    [](const testing::TestParamInfo<TwoPhaseEulerAdvectionTestDecodeStateParameters>& info) { return std::to_string(info.index); });
