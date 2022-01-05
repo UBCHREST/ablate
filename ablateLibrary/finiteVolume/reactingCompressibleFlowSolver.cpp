fab7b0d297691af40e38610597c94a9cf64c11a9
Matt McGurn <matt@mcgurn.dev>
2022-01-05 16:59:45 -0500

    added additionalProcesses to reactingCompressibleFlowSolver

--- ablateLibrary/finiteVolume/reactingCompressibleFlowSolver.cpp
+++ ablateLibrary/finiteVolume/reactingCompressibleFlowSolver.cpp
@@ -1,14 +1,18 @@
 #include "reactingCompressibleFlowSolver.hpp"
-#include <finiteVolume/processes/tChemReactions.hpp>
+#include "finiteVolume/processes/tChemReactions.hpp"
+#include "utilities/vectorUtilities.hpp"
 
 ablate::finiteVolume::ReactingCompressibleFlowSolver::ReactingCompressibleFlowSolver(std::string solverId, std::shared_ptr<domain::Region> region, std::shared_ptr<parameters::Parameters> options,
                                                                                      std::shared_ptr<eos::EOS> eosIn, std::shared_ptr<parameters::Parameters> parameters,
                                                                                      std::shared_ptr<eos::transport::TransportModel> transport,
                                                                                      std::shared_ptr<fluxCalculator::FluxCalculator> fluxCalculatorIn,
-                                                                                     std::vector<std::shared_ptr<boundaryConditions::BoundaryCondition>> boundaryConditions)
+                                                                                     std::vector<std::shared_ptr<boundaryConditions::BoundaryCondition>> boundaryConditions,
+                                                                                     std::vector<std::shared_ptr<processes::Process>> additionalProcesses)
     : CompressibleFlowSolver(solverId, region, options, eosIn, parameters, transport, fluxCalculatorIn,
-                             {std::make_shared<ablate::finiteVolume::processes::TChemReactions>(
-                                 std::dynamic_pointer_cast<eos::TChem>(eosIn) ? std::dynamic_pointer_cast<eos::TChem>(eosIn) : throw std::invalid_argument("The eos must of type eos::TChem"))},
+                             ablate::utilities::VectorUtilities::Merge(
+                                 {std::make_shared<ablate::finiteVolume::processes::TChemReactions>(
+                                     std::dynamic_pointer_cast<eos::TChem>(eosIn) ? std::dynamic_pointer_cast<eos::TChem>(eosIn) : throw std::invalid_argument("The eos must of type eos::TChem"))},
+                                 additionalProcesses),
                              boundaryConditions) {}
 
 #include "registrar.hpp"
@@ -18,4 +22,5 @@ REGISTER(ablate::solver::Solver, ablate::finiteVolume::ReactingCompressibleFlowS
          ARG(ablate::parameters::Parameters, "parameters", "the compressible flow parameters cfl, gamma, etc."),
          OPT(ablate::eos::transport::TransportModel, "transport", "the diffusion transport model"),
          OPT(ablate::finiteVolume::fluxCalculator::FluxCalculator, "fluxCalculator", "the flux calculator (defaults to AUSM)"),
-         OPT(std::vector<ablate::finiteVolume::boundaryConditions::BoundaryCondition>, "boundaryConditions", "the boundary conditions for the flow field"));
+         OPT(std::vector<ablate::finiteVolume::boundaryConditions::BoundaryCondition>, "boundaryConditions", "the boundary conditions for the flow field"),
+         OPT(std::vector<ablate::finiteVolume::processes::Process>, "additionalProcesses", "any additional processes besides euler/yi/ev transport and rxn"));
