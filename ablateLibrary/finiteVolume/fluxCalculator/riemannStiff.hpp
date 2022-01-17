f4cd35a3e53c1faeed14019023d6050618d5a1d7
maesemen <maesemen@buffalo.edu>
2022-01-17 18:40:21 -0500

    updated stiff EOS, riemannStiff, fluxCalcTest doesn't work

---
+++ ablateLibrary/finiteVolume/fluxCalculator/riemannStiff.hpp
@@ -0,0 +1,25 @@
+#ifndef ABLATELIBRARY_RIEMANNSTIFFR_HPP
+#define ABLATELIBRARY_RIEMANNSTIFFR_HPP
+#include <eos/eos.hpp>
+#include <memory>
+#include "fluxCalculator.hpp"
+
+namespace ablate::finiteVolume::fluxCalculator {
+/*
+ * Computes the flux by treating all surfaces as Riemann problems, different stiffened gas on left/right.
+ * Reference Chang and Liou, JCP, 2007, Appendix B
+ */
+class RiemannStiff : public fluxCalculator::FluxCalculator {
+   private:
+    static Direction RiemannStiffFluxFunction(void *, PetscReal uL, PetscReal aL, PetscReal rhoL, PetscReal pL, PetscReal uR, PetscReal aR, PetscReal rhoR, PetscReal pR, PetscReal *massFlux,
+                                             PetscReal *p12);
+    PetscReal gammaVec[4];// gamma_L, gamma_R, p0_L, p0_R
+
+   public:
+    FluxCalculatorFunction GetFluxCalculatorFunction() override { return RiemannStiffFluxFunction; }
+    void *GetFluxCalculatorContext() override { return (void *)&gammaVec; }
+    explicit RiemannStiff(std::shared_ptr<eos::EOS> eosL, std::shared_ptr<eos::EOS> eosR);
+};
+}  // namespace ablate::finiteVolume::fluxCalculator
+
+#endif  // ABLATELIBRARY_RIEMANNSTIFFR_HPP
