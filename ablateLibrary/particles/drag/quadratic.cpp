1f6e42ea6709a3445abd2480cbb17ead0f0bd8a6
Ben Trettel <github@trettel.org>
2022-02-16 16:54:54 -0500

    Started adding linear and quadratic drag functions.

--- ablateLibrary/particles/drag/quadratic.cpp
+++ ablateLibrary/particles/drag/quadratic.cpp
@@ -1,5 +1,15 @@
 #include "quadratic.hpp"
+#include "utilities/mathUtilities.hpp"
 
 void ablate::particles::drag::Quadratic::ComputeDragForce(const PetscInt dim, const PetscReal *partVel, const PetscReal *flowVel, const PetscReal muF, const PetscReal rhoF, const PetscReal partDiam, PetscReal *dragForce) {
+    //const PetscReal relVel = *partVel - *flowVel;
+    //PetscReal dragForcePrefactor = -0.44 * 0.5 * (PETSC_PI / 4.0) * partDiam * partDiam * rhoF * ablate::utilities::MathUtilities::MagVector(dim, *relVel);
     
+    //for (int n = 0; n < dim; n++) {
+        //dragForce[n] = dragForcePrefactor * (partVel[n] - flowVel[n]);
+    //}
 };
+
+// TODO: tests
+// pure partVel, pure flowVel, both
+// different dimensions: 1, 2, 3
