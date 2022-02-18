1755688877e05331f973b3a83223c43f3863e5e9
Ben Trettel <github@trettel.org>
2022-02-18 12:05:33 -0500

    Fixed quadratic drag function per conversation with James Chen this morning.

--- ablateLibrary/particles/drag/quadratic.cpp
+++ ablateLibrary/particles/drag/quadratic.cpp
@@ -2,12 +2,17 @@
 #include "utilities/mathUtilities.hpp"
 
 void ablate::particles::drag::Quadratic::ComputeDragForce(const PetscInt dim, const PetscReal *partVel, const PetscReal *flowVel, const PetscReal muF, const PetscReal rhoF, const PetscReal partDiam, PetscReal *dragForce) {
-    PetscReal* relVel[dim];
+    PetscReal relVel[3];
     PetscReal dragForcePrefactor = -0.44 * 0.5 * (PETSC_PI / 4.0) * partDiam * partDiam * rhoF;
     
     for (int n = 0; n < dim; n++) {
-        *relVel[n]   = partVel[n] - flowVel[n];
-        dragForce[n] = dragForcePrefactor * (partVel[n] - flowVel[n]) * ablate::utilities::MathUtilities::MagVector(dim, *relVel);
+        relVel[n]   = partVel[n] - flowVel[n];
+    }
+    
+    dragForcePrefactor *= ablate::utilities::MathUtilities::MagVector(dim, relVel);
+    
+    for (int n = 0; n < dim; n++) {
+        dragForce[n] = dragForcePrefactor * relVel[n];
     }
 };
 
