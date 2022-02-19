725d934146bf8e8c912ae7ffd27b85d68d5ddea2
Ben Trettel <github@trettel.org>
2022-02-18 19:14:58 -0500

    Added drag formulas and references as comments.

--- ablateLibrary/particles/drag/linear.cpp
+++ ablateLibrary/particles/drag/linear.cpp
@@ -2,6 +2,15 @@
 
 void ablate::particles::drag::Linear::ComputeDragForce(const PetscInt dim, const PetscReal *partVel, const PetscReal *flowVel, const PetscReal muF, const PetscReal rhoF, const PetscReal partDiam,
                                                        PetscReal *dragForce) {
+    /** \brief Linear drag formula for a solid sphere at low Reynolds numbers.
+     *
+     * \details \f$\vec{F}_d = -3 \pi d^2 \mu_f \vec{V}$
+     *
+     * where \f$\vec{V}\f$ is the relative velocity: \f$\vec{V} = \vec{V}_p - \vec{V}_f\f$. The subscript p refers to a particle. The subscript f refers to the ambient fluid.
+     *
+     * References: E. Loth, “Quasi-steady shape and drag of deformable bubbles and drops,” International Journal of Multiphase Flow, vol. 34, no. 6, pp. 523–546, Jun. 2008, doi: 10.1016/j.ijmultiphaseflow.2007.08.010. (See eq. 3.)
+     */
+
     PetscReal dragForcePrefactor = -3.0 * PETSC_PI * partDiam * muF;
 
     for (int n = 0; n < dim; n++) {
