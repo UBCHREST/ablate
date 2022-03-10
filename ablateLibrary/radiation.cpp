ac3312c65e5974b9c1a518e8e5a89bd82b85795a
owenmylotte <48336778+owenmylotte@users.noreply.github.com>
2022-03-10 17:29:18 -0500

    Planning raytracing structure

---
+++ ablateLibrary/radiation.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Owen on 3/8/2022.
+//
+
+class Irradiate { //name of the ray tracing class
+   public:          //access specifier
+    //CompressibleMaterial material; //This object must be replaced with the ABLATE equivalent
+
+    public void raytrace() {
+         //Start a timer to track progress?
+         //array intensity //Represents the total radiation intensity into the current point
+         //get the position of the origin
+         //for every angle theta
+            //precalculate sin and cosine of the angles because they are used frequently
+            //for every angle phi
+                //direction = sin and things //create a direction vector in the current angle direction
+                //get the point of intersection with the boundary
+                //intensityHere = ? //need to follow the point from boundary back to the source
+                //intensity = intensity + intensityHere
+         //
+     }
+};
