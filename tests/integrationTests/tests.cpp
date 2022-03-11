176b01ee33747980d7bbd29a3d75c90a388340e0
maesemen <maesemen@buffalo.edu>
2022-03-11 10:39:39 -0500

    reformat files

--- tests/integrationTests/tests.cpp
+++ tests/integrationTests/tests.cpp
@@ -59,7 +59,7 @@ INSTANTIATE_TEST_SUITE_P(
 
         (MpiTestParameter){
             .testName = "inputs/volumeOfFluids/twoGasAdvectingDiscontinuity.yaml", .nproc = 1, .expectedOutputFile = "outputs/volumeOfFluids/twoGasAdvectingDiscontinuity.txt", .arguments = ""},
-        (MpiTestParameter){   .testName = "inputs/volumeOfFluids/waterGravity.yaml", .nproc = 1, .expectedOutputFile = "outputs/volumeOfFluids/waterGravity.txt", .arguments = ""}),
+        (MpiTestParameter){.testName = "inputs/volumeOfFluids/waterGravity.yaml", .nproc = 1, .expectedOutputFile = "outputs/volumeOfFluids/waterGravity.txt", .arguments = ""}),
     [](const testing::TestParamInfo<MpiTestParameter>& info) { return info.param.getTestName(); });
 
 INSTANTIATE_TEST_SUITE_P(ReactingFlow, IntegrationTestsSpecifier,
