77de872054a9a6aeb8e7e29a861e3f283ecab51f
Matt McGurn <matt@mcgurn.dev>
2022-04-12 13:29:35 -0400

    formatting and version bump

--- systemHacks.cmake
+++ systemHacks.cmake
@@ -1,5 +1,5 @@
 # Include system specific hacks
-if(${APPLE} AND (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "arm64") AND (${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang"))
+if("${APPLE}" AND (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "arm64") AND (${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang"))
     # check for homebrew gfortran and get path for libstdc++.dylib
     execute_process(COMMAND gfortran --print-file-name=libstdc++.dylib OUTPUT_VARIABLE LIBSTDCPP_PATH)
 
