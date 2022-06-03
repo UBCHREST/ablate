4a9ff7765248c7fb2684120c00be434ff8f5bf4d
Varun Chandola <chandola@buffalo.edu>
2022-06-03 16:40:37 -0400

    Chemtab Dynamic Source Prediction (#259)
    * removing stale copy of chemtab model
    
    * Updating chemTabModel source, model and tests
    
    * Updating ChemTab Model to satisfy test #6
    
    * Fixing formatting issues with ChemTab
    
    * Freeing allocated vectors
    
    * bumping the version
    
    * fixing version number
    
    * fixing the tests
    
    * updating code for one failing test
    
    * Updating ChemTab tests to work with arm64 machines

--- tests/ablateLibrary/chemistry/chemTabModelTests.cpp
+++ tests/ablateLibrary/chemistry/chemTabModelTests.cpp
@@ -71,7 +71,7 @@ INSTANTIATE_TEST_SUITE_P(ChemTabModelTests, ChemTabModelGetSpeciesAndProgressVar
                                                  "YiCO",    "YiCO2",   "YiHCO",  "YiCH2O", "YiCH2OH", "YiCH3O", "YiCH3OH", "YiC2H",    "YiC2H2",   "YiC2H3", "YiC2H4", "YiC2H5",   "YiC2H6", "YiHCCO",
                                                  "YiCH2CO", "YiHCCOH", "YiN",    "YiNH",   "YiNH2",   "YiNH3",  "YiNNH",   "YiNO",     "YiNO2",    "YiN2O",  "YiHNO",  "YiCN",     "YiHCN",  "YiH2CN",
                                                  "YiHCNN",  "YiHCNO",  "YiHOCN", "YiHNCO", "YiNCO",   "YiC3H7", "YiC3H8",  "YiCH2CHO", "YiCH3CHO", "YiN2",   "YiAR"},
-                             .expectedProgressVariables = {"zmix", "CPV_0", "CPV_1"}}));
+                             .expectedProgressVariables = {"zmix", "CPV_0", "CPV_1", "CPV_2", "CPV_3"}}));
 
 /*******************************************************************************************************
  * Tests for getting the Compute Mass Fractions Functions
@@ -104,17 +104,16 @@ TEST_P(ChemTabModelComputeMassFractionsFunctionFixture, ShouldComputeCorrectMass
 INSTANTIATE_TEST_SUITE_P(ChemTabModelTests, ChemTabModelComputeMassFractionsFunctionFixture,
                          testing::Values((ChemTabModelComputeMassFractionsFunctionParameters){
                              .modelPath = "inputs/chemistry/chemTabTestModel_1",
-                             .inputProgressVariables = {0, .1, .2},
-                             .expectedMassFractions = {
-                                 -0.00140725565130274,    -0.00045837323449758003, -0.00035455781428995987, -0.0013910013371491,    -0.0002487882385107601, -2.04598882374001e-06,
-                                 -0.00038961941524638996, -2.7069681346739975e-05, -0.0005482163425314899,  0.00105295943562432,    -0.0035183326637905103, -0.0005537085981488,
-                                 0.002645006946767,       0.00012232255899329998,  3.0051809915890072e-05,  -6.642647683107998e-05, 0.00087529355466593,    -0.00171778095303233,
-                                 -0.0012801298701463797,  -4.033892815447998e-05,  0.00023066407677046002,  -0.00136504469385059,   0.0015772327251534102,  -0.00078278179181311,
-                                 -0.00019032477892702,    -0.00023789371908623,    -0.00534847132631586,    -0.00107020781603896,   0.0007697984124302801,  0.0026045736831105497,
-                                 -0.0011341133827743601,  -0.00075079009863218,    0.0005390351054686101,   -0.00022974845536523,   0.00010588862332675,    -0.00019050340742513,
-                                 -0.00023533306406412,    -0.00028523556122276,    0.00014006160875666002,  -0.00032022301953658,   -0.0001956971517322,    -0.00048963887131575,
-                                 -5.431705411270023e-06,  0.00055470396011405,     0.0008781291099871601,   -4.286525508170001e-05, 8.478539365690017e-06,  -0.0022354488831264796,
-                                 0.00192641706847605,     0.00074413982307823,     0.00010748827212834991,  -7.853775655976e-05,    5.923708549286434e-05}}));
+                             .inputProgressVariables = {0, .1, .2, .2, .1},
+                             .expectedMassFractions = {0.024346507649162213,  0.03070802663635961,  -0.014177157863655775, -0.012206614867194465,  -0.009586717144079401,  -0.005967521119935231,
+                                                       -0.010875227678139601, -0.03214015967582351, 0.028280729183712428,  0.01591420682377494,    0.01276966432475931,    0.016103929505397983,
+                                                       0.02193572707541392,   0.04443951471120011,  0.0002556383726955288, -0.0010179648987351305, -0.00786316213077087,   0.002096940503499309,
+                                                       0.010562042434247647,  0.016693196037656108, 0.025797843813450662,  0.02036901883527415,    0.019451247923288117,   0.02054569299637939,
+                                                       0.020019336518559503,  0.01835249376645227,  0.021750708609175502,  0.0018026983778071494,  0.010790575842471367,   0.016842113379018253,
+                                                       0.033468867920825464,  0.018857471749292653, 0.044804958092783155,  0.13419273882405358,    -0.0037252238295265167, -0.06779158472561372,
+                                                       -0.05588082676187539,  -0.048957981486385,   -0.0984548580421235,   0.017779474312028466,   -0.025172586623901604,  -0.006983341400292928,
+                                                       -0.08826244329397101,  0.029648167069252704, 0.038806576447438,     -0.0177198587682406,    -0.09435747073550743,   0.02751871209625231,
+                                                       0.020135538237014753,  0.04394390344680865,  -0.04396782613129059,  0.051775990887332514,   0.11555003804381765}}));
 
 /*******************************************************************************************************
  * Tests for getting the Source and Source Energy Predictions
@@ -144,13 +143,17 @@ TEST_P(ChemTabModelComputeSourceFunctionFixture, ShouldComputeCorrectSource) {
     // assert
     EXPECT_FLOAT_EQ(GetParam().expectedSourceEnergy, actualSourceEnergy) << "The sourceEnergy is incorrect";
     for (std::size_t r = 0; r < actual.size(); r++) {
-        EXPECT_FLOAT_EQ(GetParam().expectedSource[r], actual[r]) << "The value for index [" << r << "] is incorrect";
+        auto percentDifference = PetscAbs((GetParam().expectedSource[r] - actual[r]) / (0.5 * (GetParam().expectedSource[r] + actual[r])));
+        ASSERT_LT(percentDifference, 5.0E-6) << " the percent difference of (" << GetParam().expectedSource[r] << ", " << actual[r] << ") should be less than 5.0E-6 for index [" << r << "] ";
+        // EXPECT_FLOAT_EQ(GetParam().expectedSource[r], actual[r]) << "The value for index [" << r << "] is incorrect";
     }
 }
 
 INSTANTIATE_TEST_SUITE_P(ChemTabModelTests, ChemTabModelComputeSourceFunctionFixture,
-                         testing::Values((ChemTabModelComputeSourceFunctionParameters){
-                             .modelPath = "inputs/chemistry/chemTabTestModel_1", .inputProgressVariables = {1.0, .1, .2}, .expectedSource = {0.0, 0, 0}, .expectedSourceEnergy = 233370.5}));
+                         testing::Values((ChemTabModelComputeSourceFunctionParameters){.modelPath = "inputs/chemistry/chemTabTestModel_1",
+                                                                                       .inputProgressVariables = {1., .1, .2, .2, .1},
+                                                                                       .expectedSource = {39.918297, -32.271481, -26.513624, 36.252228},
+                                                                                       .expectedSourceEnergy = -3.1341994e+12}));
 
 /*******************************************************************************************************
  * Tests for getting the Progress Variables
@@ -184,8 +187,7 @@ INSTANTIATE_TEST_SUITE_P(ChemTabModelTests, ChemTabModelComputeProgressVariables
                              .modelPath = "inputs/chemistry/chemTabTestModel_1",
                              .inputMassFractions = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.,  1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.,  2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7,
                                                     2.8, 2.9, 3.,  3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.,  4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.,  5.1, 5.2, 5.3},
-                             //.expectedProgressVariables = {-17.845192, 47.81491243, -58.08515966}}));
-                             .expectedProgressVariables = {0, 47.81491243, -58.08515966}}));
+                             .expectedProgressVariables = {0, 0.3306785324000005, -5.4590796122, 1.3819009245500002, 7.1005789664000005}}));
 
 /*********************************************************************************************************
  * Test for when tensorflow is not available
