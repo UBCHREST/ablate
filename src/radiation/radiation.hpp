56bc25b2f850eda088847eb6178ec3499970a206
owenmylotte <owenmylo@buffalo.edu>
2022-06-15 14:08:59 -0400

    Revert "Adaptive stepping passes tests."
    This reverts commit 4fcdd66a

--- src/radiation/radiation.hpp
+++ src/radiation/radiation.hpp
@@ -45,7 +45,6 @@ class Radiation : public solver::CellSolver, public solver::RHSFunction {  // Ce
    private:
     /// Class Methods
     void RayInit();
-    void fillRay(PetscReal xpos, PetscReal ypos, PetscReal zpos);
 
     eos::ThermodynamicTemperatureFunction absorptivityFunction;
     const std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModel;
@@ -66,13 +65,10 @@ class Radiation : public solver::CellSolver, public solver::RHSFunction {  // Ce
     const std::shared_ptr<ablate::monitors::logs::Log> log;
 
     std::vector<std::vector<std::vector<std::vector<std::vector<PetscInt>>>>> rays;  //!< Indices: Cell, angle (theta), angle(phi), space steps (Storing indices at locations)
-    std::vector<std::vector<std::vector<std::vector<std::vector<PetscReal>>>>> h;    //!< Indices: Cell, angle (theta), angle(phi), space steps (Storing indices at locations)
+    std::vector<std::vector<std::vector<std::vector<std::vector<PetscInt>>>>> h;     //!< Indices: Cell, angle (theta), angle(phi), space steps (Storing indices at locations)
     std::vector<std::vector<std::vector<std::vector<PetscReal>>>> Ij1;               //!< Indices: Cell, angle (theta), angle(phi), domains (Storing final ray intensity of last time step)
-    std::vector<std::vector<std::vector<std::vector<PetscReal>>>> Ij;                //!< Indices: Cell, angle (theta), angle(phi), domains (Storing final ray intensity)
+    std::vector<std::vector<std::vector<std::vector<PetscReal>>>> Ij;                //!< Indices: Cell, angle (theta), angle(phi), domains (Storing final ray intensity of last time step)
     std::vector<std::vector<std::vector<std::vector<PetscReal>>>> Izeros;            //!< Indices: Cell, angle (theta), angle(phi), domains (Storing final ray intensity of last time step)
-    std::vector<std::vector<std::vector<std::vector<PetscReal>>>> broadcast;    //!< Indices: Cell, angle (theta), angle(phi), [xpos ypos zpos xdir ydir zdir] location vector
-    std::vector<std::vector<std::vector<bool>>> claims;    //!< Indices: Cell, angle (theta), angle(phi), is the ray claimed by a domain or not?
-    std::vector<std::vector<std::vector<std::vector<PetscInt>>>> ranks;    //!< Indices: Cell, angle (theta), angle(phi), [xpos ypos zpos xdir ydir zdir] location vector
 
     std::vector<std::vector<std::vector<std::vector<PetscReal>>>> Krad;
     std::vector<std::vector<std::vector<std::vector<PetscReal>>>> Kones;
