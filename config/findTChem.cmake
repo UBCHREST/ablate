7a7a81e9e8bd1024e174f4faf2b2bff93e2ac306
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-06-29 12:13:47 -0400

    working on tchem

--- config/findTChem.cmake
+++ config/findTChem.cmake
@@ -2,41 +2,28 @@
 
 # check to see if the Kokkos_DIR was specified, if not build
 if (NOT (DEFINED Kokkos_DIR|CACHE{Kokkos_DIR}|ENV{Kokkos_DIR}))
-
-        ExternalProject_Add(Kokkos
-                GIT_REPOSITORY https://github.com/kokkos/kokkos.git
-                GIT_TAG 3.6.01
-                PREFIX kokkos
-                CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/kokkos/install
-                CONFIGURE_HANDLED_BY_BUILD OFF
-                )
-
-        set(Kokkos_DIR "${CMAKE_BINARY_DIR}/kokkos/install")
-        message(STATUS "Kokkos_DIR not set.  Building Kokkos and setting Kokkos_DIR: ${CMAKE_BINARY_DIR}/kokkos/install")
-        add_dependencies(ablateLibrary Kokkos)
-
-#    message(STATUS "Kokkos_DIR not set.  Downloading and building Kokkos...")
-#    FetchContent_Declare(kokkos
-#            GIT_REPOSITORY https://github.com/kokkos/kokkos.git
-#            GIT_TAG 3.6.01   # it's much better to use a specific Git revision or Git tag for reproducibility
-#            )
-#    FetchContent_MakeAvailable(kokkos)
+    message(STATUS "Kokkos_DIR not set.  Downloading and building Kokkos...")
+    FetchContent_Declare(kokkos
+            GIT_REPOSITORY https://github.com/kokkos/kokkos.git
+            GIT_TAG 3.6.01   # it's much better to use a specific Git revision or Git tag for reproducibility
+            )
+    FetchContent_MakeAvailable(kokkos)
 elseif ()
     find_package(Kokkos REQUIRED)
 endif ()
 
 # check to see if the Tines_DIR was specified, if not build
-#if (NOT (DEFINED Tines_DIR|CACHE{Tines_DIR}|ENV{Tines_DIR}))
-#    message(STATUS "Tines_DIR not set.  Downloading and building Tines...")
-#    FetchContent_Declare(tines
-#            GIT_REPOSITORY https://github.com/UBCHREST/Tines.git
-#            GIT_TAG main
-#            SOURCE_SUBDIR src
-#            )
-#    FetchContent_MakeAvailable(tines)
-#elseif ()
+if (NOT (DEFINED Tines_DIR|CACHE{Tines_DIR}|ENV{Tines_DIR}))
+    message(STATUS "Tines_DIR not set.  Downloading and building Tines...")
+    FetchContent_Declare(tines
+            GIT_REPOSITORY https://github.com/UBCHREST/Tines.git
+            GIT_TAG main
+            SOURCE_SUBDIR src
+            )
+    FetchContent_MakeAvailable(tines)
+elseif ()
     find_package(Tines REQUIRED)
-#endif ()
+endif ()
 
 # check to see if the TChem_DIR was specified, if not build
 #if (NOT (DEFINED TChem_DIR|CACHE{TChem_DIR}|ENV{TChem_DIR}))
