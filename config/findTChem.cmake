3de2a29538946de2b811ee1fd1a54d522d68d9bb
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-07-06 13:59:36 -0400

    mostly working

--- config/findTChem.cmake
+++ config/findTChem.cmake
@@ -1,7 +1,13 @@
-# Check to see if kokkos was built in with
 # check to see if the Tines_DIR was specified, if not build
-if (NOT (DEFINED Tines_DIR|CACHE{Tines_DIR}|ENV{Tines_DIR}))
+if (Tines::tines)
+    message(STATUS "Found Tines::tines target")
+elseif (NOT (DEFINED Tines_DIR|CACHE{Tines_DIR}|ENV{Tines_DIR}))
     message(STATUS "Tines_DIR not set.  Downloading and building Tines...")
+
+    # tines expects the yaml-cpp target to be at yaml
+    add_library(yaml ALIAS yaml-cpp)
+    set(TINES_ENABLE_INSTALL OFF CACHE BOOL "" FORCE)
+
     FetchContent_Declare(tines
             GIT_REPOSITORY https://github.com/UBCHREST/Tines.git
             GIT_TAG main
@@ -9,32 +15,30 @@ if (NOT (DEFINED Tines_DIR|CACHE{Tines_DIR}|ENV{Tines_DIR}))
             )
     FetchContent_MakeAvailable(tines)
     # make sure it is in the right name space
-    if(TARGET tines)
+    if (TARGET tines)
         add_library(Tines::tines ALIAS tines)
-    endif()
+    endif ()
 elseif ()
     find_package(Tines REQUIRED)
 endif ()
-
-
 
 # check to see if the TChem_DIR was specified, if not build
 if (NOT (DEFINED TChem_DIR|CACHE{TChem_DIR}|ENV{TChem_DIR}))
     message(STATUS "TChem_DIR not set.  Downloading and building TChem...")
     set(TCHEM_ENABLE_MAIN OFF CACHE BOOL "" FORCE)
+    set(TCHEM_ENABLE_EXAMPLE OFF CACHE BOOL "" FORCE)
+    set(TCHEM_ENABLE_INSTALL OFF CACHE BOOL "" FORCE)
+
     FetchContent_Declare(tchem
             GIT_REPOSITORY https://github.com/UBCHREST/TChem.git
             GIT_TAG main
             SOURCE_SUBDIR src
-
             )
     FetchContent_MakeAvailable(tchem)
-
-    FetchContent_MakeAvailable(tchem)
     # make sure it is in the right name space
-    if(TARGET tchem)
+    if (TARGET tchem)
         add_library(TChem::tchem ALIAS tchem)
-    endif()
+    endif ()
 elseif ()
     find_package(TChem REQUIRED)
 endif ()
