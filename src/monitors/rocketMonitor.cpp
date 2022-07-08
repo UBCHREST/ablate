2340519b840a2fd68308cd7c6c3a860a47d11cca
salamlobad0712 <salaml@DESKTOP-89VT3N5.localdomain>
2022-07-08 11:24:43 -0400

    rocketMonitor

--- src/monitors/rocketMonitor.cpp
+++ src/monitors/rocketMonitor.cpp
@@ -1,16 +1,126 @@
 #include "rocketMonitor.hpp"
+#include <utility>
 #include "io/interval/fixedInterval.hpp"
 #include "monitors/logs/stdOut.hpp"
 #include "monitors/logs/log.hpp"
+#include "monitor.hpp"
 
+ablate::monitors::RocketMonitor::RocketMonitor(const std::string nameIn, std::shared_ptr<domain::Region> regionIn, std::shared_ptr<domain::Region> fieldBoundaryIn, std::shared_ptr<eos::EOS> eosIn, const std::shared_ptr<logs::Log>& logIn, const std::shared_ptr<io::interval::Interval>& intervalIn)
+    : name(nameIn), region(std::move(regionIn)), fieldBoundary(std::move(fieldBoundaryIn)), eos(std::move(eosIn)), log(logIn ? logIn : std::make_shared<logs::StdOut>()), interval(intervalIn ? intervalIn : std::make_shared<io::interval::FixedInterval>()) {}
 
-ablate::monitors::RocketMonitor::RocketMonitor(std::shared_ptr<domain::Region> regionIn, std::shared_ptr<domain::Region> fieldBoundaryIn,std::shared_ptr<logs::Log> logIn, std::shared_ptr<io::interval::Interval> intervalIn)
-    : log(logIn ? logIn : std::make_shared<logs::StdOut>()), interval(intervalIn ? intervalIn : std::make_shared<io::interval::FixedInterval>()) {}
+void ablate::monitors::RocketMonitor::Register(std::shared_ptr<solver::Solver> solver) {
+    Monitor::Register(solver);
+    // use the subDomain to set up the problem
+    std::shared_ptr<ablate::domain::SubDomain> subDomain;
+
+}
 
 PetscErrorCode ablate::monitors::RocketMonitor::OutputRocket(TS ts, PetscInt step, PetscReal crtime, Vec u, void* ctx) {
+    PetscFunctionBeginUser;
+    auto monitor = (ablate::monitors::RocketMonitor*)ctx;
+
+    if (monitor->interval->Check(PetscObjectComm((PetscObject)ts), step, crtime)) {
+        auto dm = monitor->GetSolver()->GetSubDomain().GetDM(); // get the dm
+        auto auxDM = monitor->GetSolver()->GetSubDomain().GetAuxDM(); // get the aux dm
+        auto solDM = monitor->GetSolver()->GetSubDomain().GetDM(); // get the aux dm
+
+        PetscInt dim;
+        DMGetDimension(dm, &dim); // get the dimensions of the dm
+
+        const auto& fieldPressure = monitor->GetSolver()->GetSubDomain().GetField("pressure"); // get the pressure field
+        const auto& fieldEuler = monitor->GetSolver()->GetSubDomain().GetField("euler"); // get the euler field
+        PetscReal* cellPressure; // Structure to fill with pressure for specific cell
+        PetscReal* cellEuler; // Structure to fill with pressure for specific cell
+        PetscReal* conservedValues;
+        PetscReal cellPress;
+
+        const auto auxVec = monitor->GetSolver()->GetSubDomain().GetAuxVector();
+        const PetscScalar* auxArray;
+        VecGetArrayRead(auxVec, &auxArray);
+        const auto solVec = monitor->GetSolver()->GetSubDomain().GetSolutionVector();
+        const PetscScalar* solArray;
+        VecGetArrayRead(solVec, &solArray);
+
+        Vec faceGeomVec;
+        Vec cellGeomVec;
+        DMPlexComputeGeometryFVM(dm, &cellGeomVec, &faceGeomVec) >> checkError;
+        DM faceDM, cellDM;
+
+        VecGetDM(faceGeomVec, &faceDM) >> checkError;
+        VecGetDM(cellGeomVec, &cellDM) >> checkError;
+        const PetscScalar* cellGeomArray;
+        const PetscScalar* faceGeomArray;
+        VecGetArrayRead(cellGeomVec, &cellGeomArray) >> checkError;
+        VecGetArrayRead(faceGeomVec, &faceGeomArray) >> checkError;
+
+        // initialize vectors
+        PetscReal mDotCell[3] = {0,0,0};
+        PetscReal mDotTotal[3] = {0,0,0};
+        PetscReal thrustCell[3] = {0,0,0};
+        PetscReal thrustTotal[3] = {0,0,0};
+        PetscReal Isp[3] = {0,0,0};
+
+        // find all faces
+        PetscInt fStart, fEnd;
+        DMPlexGetHeightStratum(dm, 1, &fStart, &fEnd) >> checkError;
+
+        // need to look for faces at specified fieldBoundary then find cells bordering those faces which are in specified region
+        for (PetscInt face = fStart; face < fEnd; ++face) { // Iterate through all faces to check if in fieldBoundary
+            if (ablate::domain::Region::InRegion(monitor->fieldBoundary, dm, face)) { // Check if each face is in fieldBoundary
+                PetscFVFaceGeom* fg;
+                DMPlexPointLocalRead(faceDM, face, faceGeomArray, &fg) >> checkError; // read face geometry for face
+
+                PetscInt numberNeighborCells;
+                const PetscInt* neighborCells;
+                DMPlexGetSupportSize(dm, face, &numberNeighborCells) >> ablate::checkError;
+                DMPlexGetSupport(dm, face, &neighborCells) >> ablate::checkError;
+                for (PetscInt n = 0; n < numberNeighborCells; n++) {
+                    if (ablate::domain::Region::InRegion(monitor->region, dm, neighborCells[n])) {  // check if cell is in region
+                        DMPlexPointLocalFieldRead(auxDM, neighborCells[n], fieldPressure.id, auxArray, &cellPressure);  // Retrieve pressure from cell
+                        DMPlexPointLocalRead(solDM, neighborCells[n], solArray, &conservedValues);  // Retrieve pressure from cell
+                        DMPlexPointLocalFieldRead(solDM, neighborCells[n], fieldEuler.id, solArray, &cellEuler);  // Retrieve pressure from cell
+
+                        PetscFVCellGeom* cg;
+                        DMPlexPointLocalRead(cellDM, neighborCells[n], cellGeomArray, &cg);
+
+                        monitor->computePressure = monitor->eos->GetThermodynamicFunction(eos::ThermodynamicProperty::Pressure, monitor->GetSolver()->GetSubDomain().GetFields());
+                        monitor->computePressure.function(conservedValues, &cellPress, monitor->computePressure.context.get());
+
+                        for (PetscInt d = 0; d < dim; d++) {
+                            mDotCell[d] = fg->normal[d]*cellEuler[finiteVolume::CompressibleFlowFields::RHOU+d]; // calculate mass flow rate for the cell
+                            mDotTotal[d] = mDotTotal[d] + mDotCell[d]; // summation of total mass flow rate along fieldBoundary
+                            thrustCell[d] = (mDotCell[d])*((cellEuler[finiteVolume::CompressibleFlowFields::RHOU+d])/(cellEuler[finiteVolume::CompressibleFlowFields::RHO])) + (fg->normal[d])*(cellPress-101325); // calculate thrust for the cell
+                            thrustTotal[d] = thrustTotal[d] + thrustCell[d]; // summation of total trust along fieldBoundary
+                        }
+                    }
+                }
+                for (PetscInt d = 0; d < dim; d++) {
+                    if (mDotTotal[d] > 0) {
+                        Isp[d] = thrustTotal[d] / ((mDotTotal[d]) * 9.8);  // calculate specific Impulse
+                    }
+                }
+            }
+        }
+
+        if (monitor->interval->Check(PetscObjectComm((PetscObject)ts), step, crtime)) {
+            if (monitor->name != "") { // If user passed a name argument then output name
+                monitor->log->Printf("%s ", monitor->name.c_str());
+            }
+            monitor->log->Printf("RocketMonitor for timestep %04d:\n", (int)step);
+            monitor->log->Printf("\tThrust:\t [ %1.3f, %1.3f, %1.3f]\n",thrustTotal[0],thrustTotal[1],thrustTotal[2]);
+            monitor->log->Printf("\tIsp:\t [ %1.3f, %1.3f, %1.3f]\n",Isp[0],Isp[1],Isp[2]);
+        }
+
+    }
+    PetscFunctionReturn(0);
 
 }
 
 #include "registrar.hpp"
 REGISTER(ablate::monitors::Monitor, ablate::monitors::RocketMonitor, "Outputs the Thrust and Specific Impulse of a Rocket",
-         OPT(ablate::monitors::logs::Log, "log", "where to record log (default is stdout)"), OPT(ablate::io::interval::Interval, "interval", "report interval object, defaults to every"));
+         OPT(std::string, "name", "if provided this name is used to indentify the monitor"),
+         ARG(ablate::domain::Region, "region", "the region to apply this solver"),
+         ARG(ablate::domain::Region, "fieldBoundary", "the region describing the faces between the boundary and field"),
+         ARG(ablate::eos::EOS, "eos", "(ablate::eos::EOS) The EOS describing the flow field at the boundary"),
+         OPT(ablate::monitors::logs::Log, "log", "where to record log (default is stdout)"),
+         OPT(ablate::io::interval::Interval, "interval", "report interval object, defaults to every"));
