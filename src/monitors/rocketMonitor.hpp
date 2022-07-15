60d7854f085bb03330d699df47036fb7e085853c
salamlobad0712 <salaml@DESKTOP-89VT3N5.localdomain>
2022-07-15 14:42:04 -0400

    rocketMonitor added

---
+++ src/monitors/rocketMonitor.hpp
@@ -0,0 +1,35 @@
+#ifndef ABLATELIBRARY_ROCKETMONITOR_HPP
+#define ABLATELIBRARY_ROCKETMONITOR_HPP
+
+#include <petsc.h>
+#include "domain/subDomain.hpp"
+#include "domain/region.hpp"
+#include "io/interval/interval.hpp"
+#include "monitor.hpp"
+#include "monitors/logs/log.hpp"
+#include "finiteVolume/boundaryConditions/ghost.hpp"
+#include "finiteVolume/compressibleFlowFields.hpp"
+#include "finiteVolume/finiteVolumeSolver.hpp"
+#include "eos/eos.hpp"
+
+namespace ablate::monitors {
+
+class RocketMonitor : public Monitor {
+   private:
+    static PetscErrorCode OutputRocket(TS ts, PetscInt step, PetscReal crtime, Vec u, void *ctx);
+    const std::string name;
+    const std::shared_ptr<domain::Region> region;
+    const std::shared_ptr<domain::Region> fieldBoundary;
+    const std::shared_ptr<eos::EOS> eos;
+    eos::ThermodynamicFunction computePressure;
+    const std::shared_ptr<logs::Log> log;
+    const std::shared_ptr<io::interval::Interval> interval;
+
+   public:
+    RocketMonitor(const std::string name, std::shared_ptr<domain::Region> region, std::shared_ptr<domain::Region> fieldBoundary, std::shared_ptr<eos::EOS> eos, const std::shared_ptr<logs::Log>& log = {}, const std::shared_ptr<io::interval::Interval>& interval = {});
+    void Register(std::shared_ptr<solver::Solver>) override;
+    PetscMonitorFunction GetPetscFunction() override { return OutputRocket; }
+};
+
+}  // namespace ablate::monitors
+#endif  // ABLATELIBRARY_ROCKETMONITOR_HPP
