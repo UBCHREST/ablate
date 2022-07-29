d7b35befbcee8941348e1f40781d6323b418a4f5
jpdening <jpdening@buffalo.edu>
2022-07-28 23:09:48 -0400

    Ressurected my poor deceased monitor.

--- src/monitors/turbFlowStats.cpp
+++ src/monitors/turbFlowStats.cpp
@@ -1,16 +1,15 @@
 #include "monitors/turbFlowStats.hpp"
-#include "monitors/logs/stdOut.hpp"
 #include "io/interval/fixedInterval.hpp"
 #include "petscmath.h"
 #include "solver/range.hpp"
 
-using ttf = ablate::eos::ThermodynamicFunction;
+
 using tp = ablate::eos::ThermodynamicProperty;
 using fLoc =  ablate::domain::FieldLocation;
 typedef ablate::solver::Range Range;
 
-ablate::monitors::TurbFlowStats::TurbFlowStats(const std::string& nameIn, const std::shared_ptr<ablate::eos::EOS> eosIn, std::shared_ptr<logs::Log> logIn, std::shared_ptr<io::interval::Interval> intervalIn)
-    : fieldName(nameIn), eos(eosIn), log(logIn ? logIn : std::make_shared<logs::StdOut>()), interval(intervalIn ? intervalIn : std::make_shared<io::interval::FixedInterval>()){}
+ablate::monitors::TurbFlowStats::TurbFlowStats(const std::vector<std::string> nameIn, const std::shared_ptr<ablate::eos::EOS> eosIn, std::shared_ptr<io::interval::Interval> intervalIn)
+    : fieldNames(nameIn), eos(eosIn), interval(intervalIn ? intervalIn : std::make_shared<io::interval::FixedInterval>()){}
 
 PetscErrorCode ablate::monitors::TurbFlowStats::MonitorTurbFlowStats(TS ts, PetscInt step, PetscReal crtime, Vec u, void* ctx) {
     PetscFunctionBeginUser;
@@ -20,34 +19,25 @@ PetscErrorCode ablate::monitors::TurbFlowStats::MonitorTurbFlowStats(TS ts, Pets
     auto monitor = (ablate::monitors::TurbFlowStats*)ctx;
 
     if (monitor->interval->Check(PetscObjectComm((PetscObject)ts), step, crtime)) {
-        const auto& field = monitor->GetSolver()->GetSubDomain().GetField(monitor->fieldName);
-        IS vecIS;
-        DM subDM, solDM;
-        Vec vec, solVec;
+        std::vector<Vec> vec(monitor->fieldNames.size(), nullptr);
+        std::vector<IS> vecIS(monitor->fieldNames.size(), nullptr);
+        std::vector<DM> subDM(monitor->fieldNames.size(), nullptr);
+        for(int f = 0; f < (int)monitor->fieldNames.size(); f++) {
+            const auto& field = monitor->GetSolver()->GetSubDomain().GetField(monitor->fieldNames[f]);
 
-        ierr = monitor->GetSolver()->GetSubDomain().GetFieldGlobalVector(field, &vecIS, &vec, &subDM);
-        CHKERRQ(ierr);
+            ierr = monitor->GetSolver()->GetSubDomain().GetFieldGlobalVector(field, &vecIS[f], &vec[f], &subDM[f]);
+            CHKERRQ(ierr);
+        }
 
+        DM solDM;
+        Vec solVec;
         solVec = monitor->GetSolver()->GetSubDomain().GetSubSolutionVector();
         solDM = monitor->GetSolver()->GetSubDomain().GetDM();
 
-        MPI_Comm comm = PetscObjectComm((PetscObject)vec);
-        if (monitor->log->Initialized()) {
-            monitor->log->Initialize(comm);
-        }
-
         //! Get relevant data
-        // Get density thermodynamic function
-        ttf densityFunc = monitor->eos->GetThermodynamicFunction(tp::Density, monitor->GetSolver()->GetSubDomain().GetFields(fLoc::SOL));
-
         // Get cell range (just keep data in this struct and read out as necessary).
         Range cellRange;
         monitor->GetSolver()->GetCellRange(cellRange);
-
-        // Extract the field vector global array
-        const PetscScalar* fieldDat;
-        ierr = VecGetArrayRead(vec, &fieldDat);
-        CHKERRQ(ierr);
 
         // Extract the solution global array
         const PetscScalar* solDat;
@@ -59,105 +49,83 @@ PetscErrorCode ablate::monitors::TurbFlowStats::MonitorTurbFlowStats(TS ts, Pets
         ierr = VecGetArray(monitor->turbVec, &turbDat);
         CHKERRQ(ierr);
 
-        //! Compute measures
         // Get the timestep from the TS
         PetscReal dt;
         ierr = TSGetTimeStep(ts, &dt);
         CHKERRQ(ierr);
 
-        // Instantiate running sums needed for RMS, MRMS
-        std::vector<double> sum(field.numberComponents, 0.0);
-        std::vector<double> sum2(field.numberComponents, 0.0);
-        std::vector<double> mSum(field.numberComponents, 0.0);
-        std::vector<double> mSum2(field.numberComponents, 0.0);
-        double densSum = 0.0;
-
-        // REMOVE THIS!!!
-        VecView(monitor->turbVec, PETSC_VIEWER_STDOUT_WORLD);
-
-        for (int c = cellRange.start; c < cellRange.end; c++) {
-            const PetscScalar* fieldPt;
-            const PetscScalar* solPt;
-            PetscScalar* turbPt;
-
-            // Get field point data
-            ierr = DMPlexPointLocalRead(subDM, cellRange.points[c], fieldDat, &fieldPt);
+        //! Iterator guide
+        //f - field iterator
+        //c - cell iterator
+        //p - field component iterator
+        for(int f = 0; f < (int)monitor->fieldNames.size(); f++) {
+            // Extract the field vector global array
+            const PetscScalar* fieldDat;
+            ierr = VecGetArrayRead(vec[f], &fieldDat);
             CHKERRQ(ierr);
 
-            // Get solution point data
-            ierr = DMPlexPointLocalRead(solDM, cellRange.points[c], solDat, &solPt);
-            CHKERRQ(ierr);
+            //! Compute measures
+            for (int c = cellRange.start; c < cellRange.end; c++) {
+                const PetscScalar* fieldPt;
+                const PetscScalar* solPt;
+                PetscScalar* turbPt;
 
-            // Get read/write access to point in turbulent flow array
-            ierr = DMPlexPointLocalRef(monitor->turbDM, cellRange.points[c], turbDat, &turbPt);
-            CHKERRQ(ierr);
+                // Get field point data
+                ierr = DMPlexPointLocalRead(subDM[f], cellRange.points[c], fieldDat, &fieldPt);
+                CHKERRQ(ierr);
 
-            // Get the density data from solution point data
-            PetscReal densLoc;
-            densityFunc.function(solPt, &densLoc, densityFunc.context.get());
+                // Get solution point data
+                ierr = DMPlexPointLocalRead(solDM, cellRange.points[c], solDat, &solPt);
+                CHKERRQ(ierr);
 
-            // Perform actual calculations now
-            // Add density*dt at this point to the running sum
-            turbPt[FieldOffset::dSum] += densLoc * dt;
+                // Get read/write access to point in turbulent flow array
+                ierr = DMPlexPointGlobalRef(monitor->turbDM, cellRange.points[c], turbDat, &turbPt);
+                CHKERRQ(ierr);
 
-            // Iterate through the components of the field values at this point
-            for (int j = 0; j < field.numberComponents; j++) {
-                // Multiply field value by density*dt
-                turbPt[FieldOffset::dMult + j] += densLoc * fieldPt[j] * dt;
+                if(turbPt) {
+                    // Get the density data from solution point data
+                    PetscReal densLoc;
+                    monitor->densityFunc.function(solPt, &densLoc, monitor->densityFunc.context.get());
 
-                // Add field value to spatial running sum
-                sum[j] += fieldPt[j];
+                    // Perform actual calculations now
+                    turbPt[monitor->FieldOffset.densitySum] += densLoc;
+                    turbPt[monitor->FieldOffset.densityDtSum] += densLoc * dt;
 
-                // Square field value
-                sum2[j] += PetscPowReal(fieldPt[j], 2);
+                    // Initialize the iterator that will march over each field component in each field
+                    int num = monitor->fieldTrack[f];
+                    for (int p = 0; p < monitor->fieldComps[f]; p++) {
+                        num += p;
 
-                // Add density multiplied field value to running sum
-                mSum[j] += fieldPt[j] * densLoc;
-
-                // Multiply sqare field value by density
-                mSum2[j] += sum2[j] * densLoc;
+                        turbPt[monitor->FieldOffset.densityMult + num] += fieldPt[p] * densLoc;
+                        turbPt[monitor->FieldOffset.densityDtMult + num] += fieldPt[p] * densLoc * dt;
+                        turbPt[monitor->FieldOffset.densitySqr + num] += fieldPt[p] * fieldPt[p] * densLoc;
+                        turbPt[monitor->FieldOffset.sum + num] += fieldPt[p];
+                        turbPt[monitor->FieldOffset.sumSqr + num] += fieldPt[p] * fieldPt[p];
+                        turbPt[monitor->FieldOffset.favreAvg + num] += turbPt[monitor->FieldOffset.densityDtMult + num] / (turbPt[monitor->FieldOffset.densityDtSum] + monitor->tiny);
+                        turbPt[monitor->FieldOffset.rms + num] =
+                            PetscSqrtReal(turbPt[monitor->FieldOffset.sumSqr + num] / (step + monitor->tiny) - PetscPowReal(turbPt[monitor->FieldOffset.sum + num] / (step + monitor->tiny), 2));
+                        turbPt[monitor->FieldOffset.mRms + num] =
+                            PetscSqrtReal(turbPt[monitor->FieldOffset.densitySqr + num] / (turbPt[monitor->FieldOffset.densitySum] + monitor->tiny) -
+                                          PetscPowReal(turbPt[monitor->FieldOffset.densityMult + num] / (turbPt[monitor->FieldOffset.densitySum] + monitor->tiny), 2));
+                    }
+                }
             }
-
-            // Add density to spatial running sum
-            densSum += densLoc;
         }
-
         // Free the turbulent flow array
         ierr = VecRestoreArray(monitor->turbVec, &turbDat);
         CHKERRQ(ierr);
-
-        // REMOVE THIS!!!
-        VecView(monitor->turbVec, PETSC_VIEWER_STDOUT_WORLD);
-
-        // Calculate RMS and mRMS
-        std::vector<double> rms(field.numberComponents, 0.0);
-        std::vector<double> mRms(field.numberComponents, 0.0);
-
-        for (int c = 0; c < field.numberComponents; c++) {
-            rms[c] = PetscSqrtReal(sum2[c] / (cellRange.end - cellRange.start + tiny) - PetscPowReal(sum[c] / (cellRange.end - cellRange.start + tiny), 2));
-            mRms[c] = PetscSqrtReal(mSum2[c] / (densSum + tiny) - PetscPowReal(mSum[c] / (densSum + tiny), 2));
-        }
-
-        // Output the RMS and mRMS data to the log
-        monitor->log->Printf("Turb Flow Stats\n");
-        monitor->log->Print("RMS", rms.size(), &rms[0], "%3.2e");
-        monitor->log->Print("\nMRMS", mRms.size(), &mRms[0], "%3.2e");
-        monitor->log->Printf("\n");
-
-        PetscFunctionReturn(0);
     }
+    PetscFunctionReturn(0);
 }
 
-void ablate::monitors::TurbFlowStats::AddField(DM &dm, const char* nameField, const char* nameRegion, PetscInt numComp) {
+void ablate::monitors::TurbFlowStats::AddField(DM &dm, const char* nameField, PetscInt numComp) {
     PetscFV fvm;
     PetscFVCreate(PetscObjectComm(PetscObject(dm)), &fvm) >>checkError;
     PetscObjectSetName((PetscObject)fvm, nameField) >>checkError;
     PetscFVSetFromOptions(fvm) >>checkError;
     PetscFVSetNumComponents(fvm, numComp) >>checkError;
 
-    DMLabel thisRegion;
-    DMGetLabel(dm, nameRegion, &thisRegion) >>checkError;
-    DMAddField(dm, thisRegion, (PetscObject)fvm) >>checkError;
+    DMAddField(dm, nullptr, (PetscObject)fvm) >>checkError;
     PetscFVDestroy(&fvm);
 }
 
@@ -167,27 +135,132 @@ void ablate::monitors::TurbFlowStats::Register(std::shared_ptr<ablate::solver::S
     ablate::monitors::Monitor::Register(solverIn);
 
     //Copy the master DM over, set fields, and initialize vector values
-    DM coordDM;
-    DMGetCoordinateDM(this->GetSolver()->GetSubDomain().GetDM(), &coordDM) >>checkError;
+    DMPlexFilter(this->GetSolver()->GetSubDomain().GetDM(), this->GetSolver()->GetSubDomain().GetLabel(), 1, &turbDM);
+    std::string dmName = "TurbDM";
+    PetscObjectSetName((PetscObject)turbDM, dmName.c_str());
 
-    DMClone(this->GetSolver()->GetSubDomain().GetDM(), &turbDM);
-    DMSetCoordinateDM(turbDM, coordDM) >>checkError;
+    //Add the densitySum and densityDtSum fields
+    std::string densitySum = "rhoSum";
+    AddField(turbDM, densitySum.c_str(), 1);
 
-    PetscInt numComp = this->GetSolver()->GetSubDomain().GetField(fieldName).numberComponents;
-    //MUST include densitySum first, then densityMult
-    std::string densitySum = "densitySum";
-    AddField(turbDM, densitySum.c_str(), this->GetSolver()->GetRegion()->GetName().c_str(), 1);
-    std::string densityMult = "densityMult";
-    AddField(turbDM, densityMult.c_str(), this->GetSolver()->GetRegion()->GetName().c_str(), numComp);
+    std::string densityDtSum = "rhoDtSum";
+    AddField(turbDM, densityDtSum.c_str(), 1);
+
+    //Initialize the numComps variable, which will store the sum of all the fields
+    PetscInt numComp = 2;
+
+    //Resize the numComps and fieldTrack vectors to be the same size as the number of fields
+    fieldComps.resize(fieldNames.size(), 0);
+    fieldTrack.resize(fieldNames.size(), 0);
+    fieldTrack[0] = 0;
+
+    //Need to find numComps across all fields here
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        const auto& field = this->GetSolver()->GetSubDomain().GetField(fieldNames[f]);
+        std::string densityMult = "rhoMult_" + fieldNames[f];
+        AddField(turbDM, densityMult.c_str(), numComp);
+
+        numComp += field.numberComponents;
+        fieldComps[f] = field.numberComponents;
+        if((f+1) < (int)fieldNames.size()) {
+            fieldTrack[f + 1] = fieldTrack[f] + field.numberComponents;
+        }
+    }
+
+    //Set the mode of FieldOffset and FieldOrder
+    FieldOffset.SetMode(numComp);
+    FieldOrder.SetMode((int)fieldNames.size());
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string densityDtMult = "rhoDtMult_" + fieldNames[f];
+        AddField(turbDM, densityDtMult.c_str(), numComp);
+    }
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string densitySqr = "rhoSqr_" + fieldNames[f];
+        AddField(turbDM, densitySqr.c_str(), numComp);
+    }
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string sum = "sum_" + fieldNames[f];
+        AddField(turbDM, sum.c_str(), numComp);
+    }
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string sumSqr = "sumSqr_" + fieldNames[f];
+        AddField(turbDM, sumSqr.c_str(), numComp);
+    }
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string favreAvg = "favreAvg_" + fieldNames[f];
+        AddField(turbDM, favreAvg.c_str(), numComp);
+    }
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string rms = "rms_" + fieldNames[f];
+        AddField(turbDM, rms.c_str(), numComp);
+    }
+
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        std::string mRms = "mRms_" + fieldNames[f];
+        AddField(turbDM, mRms.c_str(), numComp);
+    }
+
+    //Create the PetscSection
+    PetscSection turbSection;
+    DMGetLocalSection(turbDM, &turbSection);
+
+    //Register densitySum and densityDtSum with the PetscSection
+    PetscSectionSetComponentName(turbSection, FieldOrder.densitySum, 0, densitySum.c_str());
+    PetscSectionSetComponentName(turbSection, FieldOrder.densityDtSum, 0, densityDtSum.c_str());
+
+    //Register all component-number-dependent fields
+    for(int f = 0; f < (int)fieldNames.size(); f++) {
+        const auto& field = this->GetSolver()->GetSubDomain().GetField(fieldNames[f]);
+        for(int p = 0; p < field.numberComponents; p++) {
+            PetscSectionSetComponentName(turbSection, FieldOrder.densityMult + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.densityDtMult + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.densitySqr + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.sum + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.sumSqr + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.favreAvg + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.rms + f, p, field.components[p].c_str());
+            PetscSectionSetComponentName(turbSection, FieldOrder.mRms + f, p, field.components[p].c_str());
+        }
+    }
+
+    //!Replace name with id here
+    //Name the PetscSection
+    PetscObjectSetName((PetscObject)turbSection, name.c_str());
 
     DMCreateGlobalVector(turbDM, &turbVec);
+
+    std::string turbName = "Turb";
+    PetscObjectSetName((PetscObject)turbVec, turbName.c_str());
     VecSet(turbVec, 0.0);
+
+    //Get the density thermodynamic function
+    densityFunc = eos->GetThermodynamicFunction(tp::Density, this->GetSolver()->GetSubDomain().GetFields(fLoc::SOL));
 }
 
+void ablate::monitors::TurbFlowStats::Save(PetscViewer viewer, PetscInt sequenceNumber, PetscReal time) {
+    if(sequenceNumber == 0) {
+        DMView(turbDM, viewer) >>checkError;
+    }
+
+    DMSetOutputSequenceNumber(turbDM, sequenceNumber, time);
+
+    VecView(turbVec, viewer) >>checkError;
+}
+
+void ablate::monitors::TurbFlowStats::Restore(PetscViewer viewer, PetscInt sequenceNumber, PetscReal time) {
+
+    DMSetOutputSequenceNumber(turbDM, sequenceNumber, time);
+    VecLoad(turbVec, viewer) >>checkError;
+}
 
 #include <registrar.hpp>
 REGISTER(ablate::monitors::Monitor, ablate::monitors::TurbFlowStats, "Computes turbulent flow statistics",
-         ARG(std::string, "field", "The name of the field"),
+         ARG(std::vector<std::string>, "fields", "The name of the field"),
          ARG(ablate::eos::EOS, "eos", "The equation of state"),
-         OPT(ablate::monitors::logs::Log, "log", "Where the data will be sent (default is stdout)"),
          OPT(ablate::io::interval::Interval, "interval", "The monitor output interval"));
