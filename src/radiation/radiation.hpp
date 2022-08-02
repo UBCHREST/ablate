397b299325505ace8f7b54d716e20783c67c550a
owenmylotte <48336778+owenmylotte@users.noreply.github.com>
2022-08-02 16:52:07 -0400

    PETSc Particle Radiation (#294)
    * Added adaptive stepping.
    
    * Adaptive stepping passes tests.
    
    * Put in comment sketch for parallel cell search and solve.
    
    * Working on parallel algorithm
    
    * Still working on parallel
    
    * Revert "Adaptive stepping passes tests."
    
    This reverts commit 4fcdd66a
    
    * Organizing structure of particle search and solve method
    
    * Beginning particle conversion
    
    * Fully planned particle based solver.
    
    * Restructured radiation search.
    
    * It runs!
    
    * Might work.
    
    * Field array indexing?
    
    * Rebuilding from the ashes.
    
    * Mostly rebuilt. Quite happy with map/struct storage of data.
    
    * Some cleanup and planning.
    
    * Nearly finished parallel solve step setup.
    
    * Ready to start debugging particle radiation.
    
    * Problem with adding particles to existing fields.
    
    * Fixed particle adding problem.
    
    * Origin doesn't write properly.
    
    * Cells write intensities correctly. (Replaced map key with a string id) Checking to see if values are correct.
    
    * Error lower. Checking MPI.
    
    * Passes tests in serial.
    
    * Fixing asynchronous behavior in initialization.
    
    * So close.
    
    * Fixed initialization errors.
    
    * Fixed segment forming errors. Fixed carrier information errors. Working on rank # differences and value errors.
    
    * Fixed rank differences by properly handling ghost cells. Working on field communicators / vectors and rank issues in solve.
    
    * Found temperature field error in GetFieldLocalVector -> VecGetSubVector -> PetscCheckSameComm where IS and AUX vec do not have the same comm.
    
    * Runs through solve with multiple processes. Solution is off by an order of magnitude.
    
    * Fixing issue with segment ordering. Doesn't grab last segment.
    
    * One ray works!
    
    * MPI works!
    
    * Some cleanup.
    
    * More cleanup.
    
    * Fixed matching of integration test output file.
    
    * Formatting cleanup.
    
    * Formatting cleanup.
    
    * More cleanup.
    
    * More cleanup.
    
    * More cleanup.
    
    * More cleanup.
    
    * Documentation
    
    * Documentation
    
    * Documentation and output
    
    * Reworking information transfer / segment search for global computation.
    
    * Finished changing information transfer method. Ready to test.
    
    * Fixed scaling.
    
    * Getting ready for larger scaling tests.
    
    * Documentation.
    
    * Documentation.
    
    * Documentation.
    
    * Documentation.
    
    * Debugging for quartz.
    
    * Debugging for quartz.
    
    * Debugging outputs for Quartz.
    
    * Debugging outputs for Quartz.
    
    * Formatting
    
    * Formatting
    
    * Switch to PETSc print.
    
    * Debugging search particle anomalies.
    
    * Fixed error? Quartz test.
    
    * Still looking
    
    * Fixed error by manual delete. Ghost cell labels likely still causing issues.
    
    * Hunting
    
    * Finishing documentation. Version number. Formatting.
    
    * Typo with log events.
    
    * Finishing documentation. Version number. Formatting.
    
    * Cleanup
    
    * Fixed cell skipping problem
    
    * Preparing to implement face stepping.
    
    * Putting face stepping together
    
    * More face stepping
    
    * Revert to stable
    
    * Splitting into multiple solvers
    
    * Need to destroy search and solve particle DM
    
    * PR ready
    
    * Version
    
    * Requested changes
    
    * Formatting
    
    * Class wrap change
    
    * Warnings
    
    Co-authored-by: owenmylotte <owenmylo@buffalo.edu>

--- src/radiation/radiation.hpp
+++ src/radiation/radiation.hpp
@@ -8,10 +8,14 @@
 #include "monitors/logs/log.hpp"
 #include "solver/cellSolver.hpp"
 #include "solver/timeStepper.hpp"
+#include "utilities/loggable.hpp"
 
 namespace ablate::radiation {
 
-class Radiation : public solver::CellSolver, public solver::RHSFunction {  // Cell solver provides cell based functionality, right hand side function compatibility with finite element/ volume
+class Radiation : public solver::CellSolver,
+                  public solver::RHSFunction,
+                  public utilities::Loggable<Radiation> {  //!< Cell solver provides cell based functionality, right hand side function compatibility with
+                                                           //!< finite element/ volume, loggable allows for the timing and tracking of events
    public:
     /**
      *
@@ -22,6 +26,8 @@ class Radiation : public solver::CellSolver, public solver::RHSFunction {  // Ce
      */
     Radiation(std::string solverId, std::shared_ptr<domain::Region> region, PetscInt raynumber, std::shared_ptr<parameters::Parameters> options,
               std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModel, std::shared_ptr<ablate::monitors::logs::Log> = {});
+
+    ~Radiation() override;
 
     /** Returns the black body intensity for a given temperature and emissivity*/
     static PetscReal FlameIntensity(PetscReal epsilon, PetscReal temperature);
@@ -42,29 +48,98 @@ class Radiation : public solver::CellSolver, public solver::RHSFunction {  // Ce
      */
     PetscErrorCode ComputeRHSFunction(PetscReal time, Vec locXVec, Vec locFVec) override;
 
+   protected:
+    DM dmcell;
+    DM radsolve;   //!< DM associated with the radiation particles
+    DM radsearch;  //!< DM which the search particles occupy
+
    private:
+    /// Structs to hold information
+
+    /** Carriers are attached to the solve particles and bring ray information from the local segments to the origin cells
+     * They are transported directly from the segment to the origin. They carry only the values that the Segment computes and not the spatial information necessary to  */
+    struct Carrier {
+        PetscReal Ij = 0;    //!< Black body source for the segment. Make sure that this is reset every solve after the value has been transported.
+        PetscReal Krad = 1;  //!< Absorption for the segment. Make sure that this is reset every solve after the value has been transported.
+        PetscReal I0 = 0;
+    };
+
+    /** Each origin cell will need to retain local information given to it by the ray segments in order to compute the final intenisty.
+     * This information will be owned by cell index and stored in a map of local cell indices.
+     * */
+    struct Origin {
+        PetscReal I0 = 0;                        //!< Determing the initial ray intensity by grabbing the head cell of the furthest ray? There will need to be additional setup for this.
+        PetscReal Isource = 0;                   //!< Value that will be contributed to by every ray segment.
+        PetscReal Kradd = 1;                     //!< Value that will be contributed to by every ray segment.
+        PetscReal intensity = 0;                 //!<  Value that will be contributed to by every ray.
+        std::map<std::string, Carrier> handler;  //!< Stores local carrier information
+    };
+
+    /** Segments belong to the local maps and hold all of the local information about the ray segments both during the search and the solve */
+    struct Segment {
+        std::vector<PetscInt> cells;  //!< Stores the cell indices of the segment locally.
+        std::vector<PetscReal> h;     //!< Stores the space steps of the segment locally.
+    };
+
+    /** Identifiers are carrying by both the search and solve particles in order to associate them with their origins and ray segments
+     * In the search particles, nsegment iterates based on how many domains the search particle has crossed.
+     * In the solve particle, nsegment remains constant as it ties the particle to its specific order in the ray */
+    struct Identifier {
+        PetscInt origin;
+        PetscInt iCell;
+        PetscInt ntheta;
+        PetscInt nphi;
+        PetscInt nsegment;
+    };
+
+    /** Virtual coordinates are used during the search to compute path length properties in case the simulation is not 3 dimensional */
+    struct Virtualcoord {
+        PetscReal x;
+        PetscReal y;
+        PetscReal z;
+        PetscReal xdir;
+        PetscReal ydir;
+        PetscReal zdir;
+        PetscInt current;
+        PetscReal hhere;
+    };
+
     /// Class Methods
     void RayInit();
+
+    /** Update the coordinates of the particle using the virtual coordinates
+     * Moves the particle in physical space instead of only updating the virtual coordinates
+     * This function must be run on every updated particle before swarm migrate is used */
+    void UpdateCoordinates(PetscInt ipart, Virtualcoord* virtualcoord, PetscReal* coord) const;
+
+    /** Create a unique identifier from an array of integers.
+     * This is done using the nested Cantor pairing function
+     * The ray segment will always be accessed by a particle carrying an identifier so it does not need to be inverted.
+     * (Unless the particles created for the solve need to find their ray segments efficiently? Maybe dont destroy the particles of the search?)
+     * */
+    std::string static inline Key(Identifier* id) {  //!< Nested Cantor pairing function in order to identify ray segment
+
+        std::string key = std::to_string(id->origin) + "." + std::to_string(id->iCell) + "." + std::to_string(id->ntheta) + "." + std::to_string(id->nphi) + "." + std::to_string(id->nsegment);
+        return key;
+    }
 
     eos::ThermodynamicTemperatureFunction absorptivityFunction;
     const std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModel;
 
-    /// Class Constants
-    const PetscReal sbc = 5.6696e-8;  // Stefan-Boltzman Constant (J/K)
-    const PetscReal pi = 3.1415926535897932384626433832795028841971693993;
+    PetscMPIInt numRanks = 0;  //!< The number of the ranks that the simulation contains. This will be used to support global indexing.
 
     /// Class inputs and Variables
-    PetscInt dim = 0;  // Number of dimensions that the domain exists within
-    PetscReal h = 0;   // This is the step size which should be set as the minimum cell radius
-    PetscInt nTheta;   // The number of angles to solve with, given by user input
-    PetscInt nPhi;     // The number of angles to solve with, given by user input (x2)
+    PetscInt dim = 0;  //!< Number of dimensions that the domain exists within
+    PetscInt nTheta;   //!< The number of angles to solve with, given by user input
+    PetscInt nPhi;     //!< The number of angles to solve with, given by user input (x2)
 
     /**
      * Store a log used to output the required information
      */
-    const std::shared_ptr<ablate::monitors::logs::Log> log;
+    const std::shared_ptr<ablate::monitors::logs::Log> log = nullptr;
 
-    std::vector<std::vector<std::vector<std::vector<PetscInt>>>> rays;  //!< Indices: Cell, angle (theta), angle(phi), space steps
+    std::map<std::string, Segment> rays;
+    std::map<PetscInt, Origin> origin;
 };
 
 }  // namespace ablate::radiation
