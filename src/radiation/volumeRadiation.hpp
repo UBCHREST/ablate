ac4c4da81737f2f2ddb16fe62341074accd17e30
owenmylotte <48336778+owenmylotte@users.noreply.github.com>
2022-08-10 15:16:04 -0400

    Radiation Abstraction (#317)
    * Going to start fresh
    
    * removed unused checkstack (#309)
    
    * Added turbulent flow statistics monitor & integration test (#311)
    
    * update physics time stepping for correct pgs usage (#312)
    
    * fixed time stepping with pgs
    
    * fixed ablate.dev status for new workflow
    
    * version bump
    
    * changed euler to NavierStokesTransport (#314)
    
    * added debug code to boundary solver (#315)
    
    * added debug code to boundary solver
    
    * fixed bug in ablateDevGenerator.py
    
    * Nearly there?
    
    * Putting in finite volume implementation.
    
    * Almost finite volume
    
    * Debugging abstraction
    
    * Running?
    
    * Error of 300000?
    
    * PR Ready
    
    * Cleanup and formatting
    
    * Cleanup and formatting
    
    * Remove unnecessary input.
    
    * xdmGenerator bump (#316)
    
    * removed unused checkstack (#309)
    
    * PR Ready
    
    * Input fix
    
    Co-authored-by: Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
    Co-authored-by: jpdening <104408374+jpdening@users.noreply.github.com>

---
+++ src/radiation/volumeRadiation.hpp
@@ -0,0 +1,37 @@
+#ifndef ABLATELIBRARY_VOLUMERADIATION_HPP
+#define ABLATELIBRARY_VOLUMERADIATION_HPP
+
+#include "radiation.hpp"
+namespace ablate::radiation {
+
+class VolumeRadiation : public Radiation, public solver::CellSolver, public solver::RHSFunction {
+   public:
+    /**
+     * Function passed into PETSc to compute the FV RHS
+     * @param dm
+     * @param time
+     * @param locXVec
+     * @param globFVec
+     * @param ctx
+     * @return
+     */
+    PetscErrorCode ComputeRHSFunction(PetscReal time, Vec locXVec, Vec locFVec) override;
+
+    void Initialize() override;
+    void Setup() override;
+    void Register(std::shared_ptr<ablate::domain::SubDomain> subDomain) override;
+
+    /**
+     *
+     * @param solverId the id for this solver
+     * @param region the boundary cell region
+     * @param rayNumber
+     * @param options other options
+     */
+    VolumeRadiation(const std::string& solverId1, const std::shared_ptr<domain::Region>& region, std::shared_ptr<domain::Region> fieldBoundary, const PetscInt raynumber,
+                    const std::shared_ptr<parameters::Parameters>& options1, std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn, std::shared_ptr<monitors::logs::Log> unnamed1);
+
+    ~VolumeRadiation();
+};
+}  // namespace ablate::radiation
+#endif  // ABLATELIBRARY_VOLUMERADIATION_HPP
