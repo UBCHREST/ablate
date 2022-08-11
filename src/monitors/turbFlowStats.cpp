328c52294ea5231671f638dc06ecf1bd4a828beb
jpdening <jpdening@buffalo.edu>
2022-08-11 10:53:24 -0400

    Changed turbFlowStats monitor to use the new subDomain function to create the turbDM.

--- src/monitors/turbFlowStats.cpp
+++ src/monitors/turbFlowStats.cpp
@@ -137,7 +137,7 @@ void ablate::monitors::TurbFlowStats::Register(std::shared_ptr<ablate::solver::S
     ablate::monitors::Monitor::Register(solverIn);
 
     // Copy the master DM over, set fields, and initialize vector values
-    DMPlexFilter(this->GetSolver()->GetSubDomain().GetDM(), this->GetSolver()->GetSubDomain().GetLabel(), 1, &turbDM);
+    this->GetSolver()->GetSubDomain().CheckSubDM(&turbDM);
     std::string DmName = "TurbDM";
     PetscObjectSetName((PetscObject)turbDM, DmName.c_str());
 
