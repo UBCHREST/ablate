2d1e685321346f869dfb1014dba24088ce7fb031
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-08-15 12:18:40 -0400

    updated code to work before dm distribute (#322)

--- src/domain/modifiers/tagLabelInterface.cpp
+++ src/domain/modifiers/tagLabelInterface.cpp
@@ -62,73 +62,81 @@ void ablate::domain::modifiers::TagLabelInterface::Modify(DM &dm) {
     DMLabelGetStratumIS(leftLabel, leftRegion->GetValue(), &leftLabelIS) >> checkError;
 
     // Get the intersect between these two IS
-    IS leftFaceIS;
-    ISIntersect(allFacesIS, leftLabelIS, &leftFaceIS) >> checkError;
+    if (leftLabelIS) {
+        IS leftFaceIS;
+        ISIntersect(allFacesIS, leftLabelIS, &leftFaceIS) >> checkError;
 
-    // Get the face range
-    PetscInt fStart, fEnd;
-    const PetscInt *faces;
-    ISGetPointRange(leftFaceIS, &fStart, &fEnd, &faces) >> checkError;
+        // Get the face range
+        PetscInt fStart, fEnd;
+        const PetscInt *faces;
+        ISGetPointRange(leftFaceIS, &fStart, &fEnd, &faces) >> checkError;
 
-    // March over each face
-    for (PetscInt f = fStart; f < fEnd; ++f) {
-        const PetscInt face = faces ? faces[f] : f;
+        // March over each face
+        for (PetscInt f = fStart; f < fEnd; ++f) {
+            const PetscInt face = faces ? faces[f] : f;
 
-        // Determine the number of supports (for faces, the support would be the cell/element(
-        PetscInt supportSize;
-        DMPlexGetSupportSize(dm, face, &supportSize);
+            // Determine the number of supports (for faces, the support would be the cell/element(
+            PetscInt supportSize;
+            DMPlexGetSupportSize(dm, face, &supportSize);
 
-        if (supportSize == 2) {
-            // Check if any of the supports/cells are not in the label.  This will tell us if this is a boundary face
-            const PetscInt *cells;
-            DMPlexGetSupport(dm, face, &cells) >> checkError;
+            if (supportSize == 2) {
+                // Check if any of the supports/cells are not in the label.  This will tell us if this is a boundary face
+                const PetscInt *cells;
+                DMPlexGetSupport(dm, face, &cells) >> checkError;
 
-            // Check to see if 0 cell is in left
-            PetscInt zeroCellLabelLeftValue, oneCellLeftValue;
-            DMLabelGetValue(leftLabel, cells[0], &zeroCellLabelLeftValue) >> checkError;
-            DMLabelGetValue(leftLabel, cells[1], &oneCellLeftValue) >> checkError;
+                // Check to see if 0 cell is in left
+                PetscInt zeroCellLabelLeftValue, oneCellLeftValue;
+                DMLabelGetValue(leftLabel, cells[0], &zeroCellLabelLeftValue) >> checkError;
+                DMLabelGetValue(leftLabel, cells[1], &oneCellLeftValue) >> checkError;
 
-            if (zeroCellLabelLeftValue == leftRegion->GetValue()) {
-                // Check if the 1 cell is in the right region
-                PetscInt otherCellValue;
-                DMLabelGetValue(rightLabel, cells[1], &otherCellValue) >> checkError;
-                if (otherCellValue == rightRegion->GetValue()) {
-                    DMLabelSetValue(boundaryFaceLabel, face, boundaryFaceRegion->GetValue()) >> checkError;
+                if (zeroCellLabelLeftValue == leftRegion->GetValue()) {
+                    // Check if the 1 cell is in the right region
+                    PetscInt otherCellValue;
+                    DMLabelGetValue(rightLabel, cells[1], &otherCellValue) >> checkError;
+                    if (otherCellValue == rightRegion->GetValue()) {
+                        DMLabelSetValue(boundaryFaceLabel, face, boundaryFaceRegion->GetValue()) >> checkError;
 
-                    if (leftBoundaryCellLabel) {
-                        DMLabelSetValue(leftBoundaryCellLabel, cells[0], leftBoundaryCellValue) >> checkError;
+                        if (leftBoundaryCellLabel) {
+                            DMLabelSetValue(leftBoundaryCellLabel, cells[0], leftBoundaryCellValue) >> checkError;
+                        }
+                        if (rightBoundaryCellLabel) {
+                            DMLabelSetValue(rightBoundaryCellLabel, cells[1], rightBoundaryCellValue) >> checkError;
+                        }
                     }
-                    if (rightBoundaryCellLabel) {
-                        DMLabelSetValue(rightBoundaryCellLabel, cells[1], rightBoundaryCellValue) >> checkError;
-                    }
-                }
-            } else if (oneCellLeftValue == leftRegion->GetValue()) {
-                // check if the 1 cell is in the left region
-                PetscInt otherCellValue;
-                DMLabelGetValue(rightLabel, cells[0], &otherCellValue) >> checkError;
-                if (otherCellValue == rightRegion->GetValue()) {
-                    DMLabelSetValue(boundaryFaceLabel, face, boundaryFaceRegion->GetValue()) >> checkError;
+                } else if (oneCellLeftValue == leftRegion->GetValue()) {
+                    // check if the 1 cell is in the left region
+                    PetscInt otherCellValue;
+                    DMLabelGetValue(rightLabel, cells[0], &otherCellValue) >> checkError;
+                    if (otherCellValue == rightRegion->GetValue()) {
+                        DMLabelSetValue(boundaryFaceLabel, face, boundaryFaceRegion->GetValue()) >> checkError;
 
-                    if (leftBoundaryCellLabel) {
-                        DMLabelSetValue(leftBoundaryCellLabel, cells[1], leftBoundaryCellValue) >> checkError;
-                    }
-                    if (rightBoundaryCellLabel) {
-                        DMLabelSetValue(rightBoundaryCellLabel, cells[0], rightBoundaryCellValue) >> checkError;
+                        if (leftBoundaryCellLabel) {
+                            DMLabelSetValue(leftBoundaryCellLabel, cells[1], leftBoundaryCellValue) >> checkError;
+                        }
+                        if (rightBoundaryCellLabel) {
+                            DMLabelSetValue(rightBoundaryCellLabel, cells[0], rightBoundaryCellValue) >> checkError;
+                        }
                     }
                 }
             }
         }
+        ISRestorePointRange(leftFaceIS, &fStart, &fEnd, &faces) >> checkError;
+        ISDestroy(&leftFaceIS);
+        ISDestroy(&leftLabelIS);
     }
-    DistributeLabel(dm, boundaryFaceLabel);
-    if (rightBoundaryCellLabel) {
-        DistributeLabel(dm, rightBoundaryCellLabel);
+
+    // check to see if we should distribute the labels
+    PetscBool isDistributed;
+    DMPlexIsDistributed(dm, &isDistributed) >> checkError;
+    if (isDistributed) {
+        DistributeLabel(dm, boundaryFaceLabel);
+        if (rightBoundaryCellLabel) {
+            DistributeLabel(dm, rightBoundaryCellLabel);
+        }
+        if (leftBoundaryCellLabel) {
+            DistributeLabel(dm, leftBoundaryCellLabel);
+        }
     }
-    if (leftBoundaryCellLabel) {
-        DistributeLabel(dm, leftBoundaryCellLabel);
-    }
-    ISRestorePointRange(leftFaceIS, &fStart, &fEnd, &faces) >> checkError;
-    ISDestroy(&leftFaceIS);
-    ISDestroy(&leftLabelIS);
     ISDestroy(&allFacesIS);
 }
 
