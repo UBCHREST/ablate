c5ad3f8dbe45ab54fe6f6127568fb999ecac7ead
owenmylotte <owenmylotte@gmail.com>
2022-08-23 14:08:32 -0400

    Update interval functionality

--- src/radiation/volumeRadiation.hpp
+++ src/radiation/volumeRadiation.hpp
@@ -2,6 +2,8 @@
 #define ABLATELIBRARY_VOLUMERADIATION_HPP
 
 #include "radiation.hpp"
+#include "io/interval/interval.hpp"
+
 namespace ablate::radiation {
 
 class VolumeRadiation : public Radiation, public solver::CellSolver, public solver::RHSFunction {
@@ -28,12 +30,14 @@ class VolumeRadiation : public Radiation, public solver::CellSolver, public solv
      * @param rayNumber
      * @param options other options
      */
-    VolumeRadiation(const std::string& solverId1, const std::shared_ptr<domain::Region>& region, std::shared_ptr<domain::Region> fieldBoundary, const PetscInt raynumber, std::shared_ptr<void> interval,
+    VolumeRadiation(const std::string& solverId1, const std::shared_ptr<domain::Region>& region, std::shared_ptr<domain::Region> fieldBoundary, const PetscInt raynumber, std::shared_ptr<io::interval::Interval> interval,
                     const std::shared_ptr<parameters::Parameters>& options1, std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn, std::shared_ptr<monitors::logs::Log> unnamed1);
 
     ~VolumeRadiation();
 
-    std::shared_ptr<void> interval;
+    PetscErrorCode RadiationPreStep(TS ts, ablate::solver::Solver& solver);
+
+    const std::shared_ptr<io::interval::Interval> interval;
 };
 }  // namespace ablate::radiation
 #endif  // ABLATELIBRARY_VOLUMERADIATION_HPP
