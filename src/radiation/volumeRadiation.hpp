26dad721959181de04481881f91899cdc77c73cb
owenmylotte <48336778+owenmylotte@users.noreply.github.com>
2022-08-29 15:39:41 -0400

    Radiation MPI Fix (#329)
    * Looking for the source of MPI issue
    
    * Found root of face geometry issue
    
    * To run on Quartz
    
    * Temporary fix to face stepping
    
    * Guarantee particle movement in parallel
    
    * Added interval for radiation solve
    
    * Input file adjusted for interval. Version Number. Formatting.
    
    * Radiation test constructor update
    
    * Update interval functionality
    
    * Update interval functionality
    
    * Formatting
    
    * Interval fix
    
    * Test update
    
    * Test update
    
    * PR Update
    
    * AuxDM
    
    * Get rid of debugging
    
    * Possibly fixed everything
    
    * Remove debugging code
    
    * Tidy things
    
    * One adjustment
    
    * Formatting

--- src/radiation/volumeRadiation.hpp
+++ src/radiation/volumeRadiation.hpp
@@ -1,7 +1,9 @@
 #ifndef ABLATELIBRARY_VOLUMERADIATION_HPP
 #define ABLATELIBRARY_VOLUMERADIATION_HPP
 
+#include "io/interval/interval.hpp"
 #include "radiation.hpp"
+
 namespace ablate::radiation {
 
 class VolumeRadiation : public Radiation, public solver::CellSolver, public solver::RHSFunction {
@@ -29,9 +31,14 @@ class VolumeRadiation : public Radiation, public solver::CellSolver, public solv
      * @param options other options
      */
     VolumeRadiation(const std::string& solverId1, const std::shared_ptr<domain::Region>& region, std::shared_ptr<domain::Region> fieldBoundary, const PetscInt raynumber,
-                    const std::shared_ptr<parameters::Parameters>& options1, std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn, std::shared_ptr<monitors::logs::Log> unnamed1);
+                    std::shared_ptr<io::interval::Interval> interval, const std::shared_ptr<parameters::Parameters>& options1,
+                    std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn, std::shared_ptr<monitors::logs::Log> unnamed1);
 
     ~VolumeRadiation();
+
+    PetscErrorCode RadiationPreStep(TS ts);
+
+    const std::shared_ptr<io::interval::Interval> interval;
 };
 }  // namespace ablate::radiation
 #endif  // ABLATELIBRARY_VOLUMERADIATION_HPP
