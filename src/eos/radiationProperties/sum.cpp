537d8cbb6d9c45e5dbf7401315420403e9704ef0
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-10-12 12:12:40 -0400

    added sum and example radiation model

---
+++ src/eos/radiationProperties/sum.cpp
@@ -0,0 +1,65 @@
+#include "sum.hpp"
+
+ablate::eos::radiationProperties::Sum::Sum(std::vector<std::shared_ptr<ablate::eos::radiationProperties::RadiationModel>> models) : models(std::move(models)) {
+    if (this->models.empty()) {
+        throw std::invalid_argument("The model list must not be empty");
+    }
+}
+
+PetscErrorCode ablate::eos::radiationProperties::Sum::SumFunction(const PetscReal *conserved, PetscReal *property, void *ctx) {
+    PetscFunctionBeginUser;
+    auto vector = (std::vector<ThermodynamicFunction> *)ctx;
+
+    *property = 0;
+    for (const auto &[subFunction, subCtx] : *vector) {
+        PetscReal propertyTmp = 0.0;
+        PetscCall(subFunction(conserved, &propertyTmp, subCtx.get()));
+        *property += propertyTmp;
+    }
+
+    PetscFunctionReturn(0);
+}
+PetscErrorCode ablate::eos::radiationProperties::Sum::SumTemperatureFunction(const PetscReal *conserved, PetscReal temperature, PetscReal *property, void *ctx) {
+    PetscFunctionBeginUser;
+    auto vector = (std::vector<ThermodynamicTemperatureFunction> *)ctx;
+
+    *property = 0;
+    for (const auto &[subFunction, subCtx] : *vector) {
+        PetscReal propertyTmp = 0.0;
+        PetscCall(subFunction(conserved, temperature, &propertyTmp, subCtx.get()));
+        *property += propertyTmp;
+    }
+
+    PetscFunctionReturn(0);
+}
+
+ablate::eos::ThermodynamicFunction ablate::eos::radiationProperties::Sum::GetRadiationPropertiesFunction(ablate::eos::radiationProperties::RadiationProperty property,
+                                                                                                         const std::vector<domain::Field> &fields) const {
+    // Create the function
+    auto contextVector = std::make_shared<std::vector<ThermodynamicFunction>>();
+    auto function = ThermodynamicFunction{.function = SumFunction, .context = contextVector};
+
+    // add each contribution
+    for (auto &model : models) {
+        contextVector->push_back(model->GetRadiationPropertiesFunction(property, fields));
+    }
+
+    return function;
+}
+ablate::eos::ThermodynamicTemperatureFunction ablate::eos::radiationProperties::Sum::GetRadiationPropertiesTemperatureFunction(ablate::eos::radiationProperties::RadiationProperty property,
+                                                                                                                               const std::vector<domain::Field> &fields) const {
+    // Create the function
+    auto contextVector = std::make_shared<std::vector<ThermodynamicTemperatureFunction>>();
+    auto function = ThermodynamicTemperatureFunction{.function = SumTemperatureFunction, .context = contextVector};
+
+    // add each contribution
+    for (auto &model : models) {
+        contextVector->push_back(model->GetRadiationPropertiesTemperatureFunction(property, fields));
+    }
+
+    return function;
+}
+
+#include "registrar.hpp"
+REGISTER_PASS_THROUGH(ablate::eos::radiationProperties::RadiationModel, ablate::eos::radiationProperties::Sum, "sums the properties of the provided models",
+                      std::vector<ablate::eos::radiationProperties::RadiationModel>);
---
+++ tests/unitTests/eos/radiationProperties/radiationSumTests.cpp
@@ -0,0 +1,76 @@
+#include "PetscTestFixture.hpp"
+#include "eos/radiationProperties/constant.hpp"
+#include "eos/radiationProperties/sum.hpp"
+#include "gtest/gtest.h"
+
+struct RadiationSumTestParameters {
+    std::function<std::vector<std::shared_ptr<ablate::eos::radiationProperties::RadiationModel>>()> getInputModels;
+
+    std::map<ablate::eos::radiationProperties::RadiationProperty, PetscReal> expectedParameters;
+};
+
+class RadiationSumTestFixture : public testingResources::PetscTestFixture, public ::testing::WithParamInterface<RadiationSumTestParameters> {};
+
+TEST_P(RadiationSumTestFixture, ShouldComputeCorrectValueForGetRadiationPropertiesFunction) {
+    // arrange
+    auto inputModels = GetParam().getInputModels();
+    auto sumModel = std::make_shared<ablate::eos::radiationProperties::Sum>(inputModels);
+
+    for (const auto& [property, expectedValue] : GetParam().expectedParameters) {
+        // act
+        auto testFunction = sumModel->GetRadiationPropertiesFunction(property, {});
+
+        PetscReal computeValue = NAN;
+        testFunction.function(nullptr, &computeValue, testFunction.context.get());
+
+        // assert
+        ASSERT_DOUBLE_EQ(expectedValue, computeValue) << "should be correct for " << property;
+    }
+}
+
+TEST_P(RadiationSumTestFixture, ShouldComputeCorrectValueForGetRadiationPropertiesTemperatureFunction) {
+    // arrange
+    auto inputModels = GetParam().getInputModels();
+    auto sumModel = std::make_shared<ablate::eos::radiationProperties::Sum>(inputModels);
+
+    for (const auto& [property, expectedValue] : GetParam().expectedParameters) {
+        // act
+        auto testFunction = sumModel->GetRadiationPropertiesTemperatureFunction(property, {});
+
+        PetscReal computeValue = NAN;
+        testFunction.function(nullptr, 1000, &computeValue, testFunction.context.get());
+
+        // assert
+        ASSERT_DOUBLE_EQ(expectedValue, computeValue) << "should be correct for " << property;
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    RadiationSumTests, RadiationSumTestFixture,
+    testing::Values(
+        (RadiationSumTestParameters){.getInputModels =
+                                         []() {
+                                             return std::vector<std::shared_ptr<ablate::eos::radiationProperties::RadiationModel>>{std::make_shared<ablate::eos::radiationProperties::Constant>(1.4),
+                                                                                                                                   std::make_shared<ablate::eos::radiationProperties::Constant>(1.6)};
+                                         },
+                                     .expectedParameters = {{ablate::eos::radiationProperties::RadiationProperty::Absorptivity, 3.0}}},
+        (RadiationSumTestParameters){
+            .getInputModels = []() { return std::vector<std::shared_ptr<ablate::eos::radiationProperties::RadiationModel>>{std::make_shared<ablate::eos::radiationProperties::Constant>(1.4)}; },
+            .expectedParameters = {{ablate::eos::radiationProperties::RadiationProperty::Absorptivity, 1.4}}},
+        (RadiationSumTestParameters){.getInputModels =
+                                         []() {
+                                             return std::vector<std::shared_ptr<ablate::eos::radiationProperties::RadiationModel>>{std::make_shared<ablate::eos::radiationProperties::Constant>(1.4),
+                                                                                                                                   std::make_shared<ablate::eos::radiationProperties::Constant>(2.4),
+                                                                                                                                   std::make_shared<ablate::eos::radiationProperties::Constant>(3.5)};
+                                         },
+                                     .expectedParameters = {{ablate::eos::radiationProperties::RadiationProperty::Absorptivity, 7.3}}}
+
+        ));
+
+TEST(RadiationSumTests, ShouldThrowExceptionForEmptyModelList) {
+    // arrange
+    std::vector<std::shared_ptr<ablate::eos::radiationProperties::RadiationModel>> models;
+
+    // act/assert
+    ASSERT_THROW(std::make_shared<ablate::eos::radiationProperties::Sum>(models);, std::invalid_argument);
+}
