f5dacc2a195160432686b1020b3a4f831fcbfd59
salamlobad0712 <salaml@DESKTOP-89VT3N5.localdomain>
2022-11-13 22:25:51 -0600

    update rocketMonitor and integration test

--- src/monitors/rocketMonitor.cpp
+++ src/monitors/rocketMonitor.cpp
@@ -19,8 +19,9 @@ ablate::monitors::RocketMonitor::RocketMonitor(const std::string nameIn, std::sh
 
 void ablate::monitors::RocketMonitor::Register(std::shared_ptr<solver::Solver> solver) {
     Monitor::Register(solver);
-    auto comm = GetSolver()->GetSubDomain().GetComm();                                                                               // The communicator in which the reduction takes place.
-    computePressure = eos->GetThermodynamicFunction(eos::ThermodynamicProperty::Pressure, GetSolver()->GetSubDomain().GetFields());  // get decode state function/context
+    auto comm = GetSolver()->GetSubDomain().GetComm();                                                                                       // The communicator in which the reduction takes place.
+    computePressure = eos->GetThermodynamicFunction(eos::ThermodynamicProperty::Pressure, GetSolver()->GetSubDomain().GetFields());          // get decode state function/context
+    computeSpeedOfSound = eos->GetThermodynamicFunction(eos::ThermodynamicProperty::SpeedOfSound, GetSolver()->GetSubDomain().GetFields());  // get decode state function/context
     if (!log->Initialized()) {
         log->Initialize(comm);
     }
@@ -47,6 +48,8 @@ PetscErrorCode ablate::monitors::RocketMonitor::OutputRocket(TS ts, PetscInt ste
         PetscReal* cellEuler;
         PetscReal* conservedValues;
         PetscReal cellPressure;
+        PetscReal cellVelocity;
+        PetscReal cellSpeedOfSound;
 
         const auto auxVec = monitor->GetSolver()->GetSubDomain().GetAuxVector();
         const PetscScalar* auxArray;
@@ -80,6 +83,23 @@ PetscErrorCode ablate::monitors::RocketMonitor::OutputRocket(TS ts, PetscInt ste
         PetscReal mDotTotalGlob[3] = {0, 0, 0};
         PetscReal thrustTotalGlob[3] = {0, 0, 0};
         PetscReal IspGlob[3] = {0, 0, 0};
+
+        // initialize field values to calculate [min max mean] for cell pressure and mach number
+        PetscReal pressureMin[1] = {100000000};
+        PetscReal pressureMax[1] = {0};
+        PetscReal pressureTotal[1] = {0};
+        PetscReal pressureMinGlob[1];
+        PetscReal pressureMaxGlob[1];
+        PetscReal pressureTotalGlob[1];
+        PetscReal machMin[1] = {100000000};
+        PetscReal machMax[1] = {0};
+        PetscReal machTotal[1] = {0};
+        PetscReal machMinGlob[1];
+        PetscReal machMaxGlob[1];
+        PetscReal machTotalGlob[1];
+        PetscReal machNumber;
+        PetscReal numCells[1] = {0};
+        PetscReal numCellsGlob[1];
 
         // find all faces
         PetscInt fStart, fEnd;
@@ -126,14 +146,51 @@ PetscErrorCode ablate::monitors::RocketMonitor::OutputRocket(TS ts, PetscInt ste
                             };
                             thrustTotal[d] = thrustTotal[d] + thrustCell[d];  // summation of total trust along fieldBoundary
                         }
+                        // update min and max cell pressure
+                        if (cellPressure < pressureMin[0]) {
+                            pressureMin[0] = cellPressure;
+                        }
+                        if (cellPressure > pressureMax[0]) {
+                            pressureMax[0] = cellPressure;
+                        }
+                        // update cell pressure sum for mean calculation
+                        pressureTotal[0] += cellPressure;
+
+                        // calculate cell mach number
+                        cellVelocity = PetscSqrtReal(PetscSqr(cellEuler[finiteVolume::CompressibleFlowFields::RHOU]) + PetscSqr(cellEuler[finiteVolume::CompressibleFlowFields::RHOU + 1]) +
+                                                     PetscSqr(cellEuler[finiteVolume::CompressibleFlowFields::RHOU + 2]));
+                        monitor->computeSpeedOfSound.function(conservedValues, &cellSpeedOfSound, monitor->computeSpeedOfSound.context.get());
+                        machNumber = cellVelocity / cellSpeedOfSound;
+
+                        // update min and max mach number
+                        if (machNumber < machMin[0]) {
+                            machMin[0] = machNumber;
+                        }
+                        if (machNumber > machMax[0]) {
+                            machMax[0] = machNumber;
+                        }
+                        // update mach number sum for mean calculation
+                        machTotal[0] += machNumber;
+                        numCells[0]++;  // Track number of cells on face in region of interest
                     }
                 }
             }
         }
 
         // Take across all ranks
+        MPI_Reduce(pressureMin, pressureMinGlob, 1, MPI_DOUBLE, MPI_MIN, 0, comm);
+        MPI_Reduce(pressureMax, pressureMaxGlob, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+        MPI_Reduce(pressureTotal, pressureTotalGlob, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
+        MPI_Reduce(machMin, machMinGlob, 1, MPI_DOUBLE, MPI_MIN, 0, comm);
+        MPI_Reduce(machMax, machMaxGlob, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+        MPI_Reduce(machTotal, machTotalGlob, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
+        MPI_Reduce(numCells, numCellsGlob, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
         MPI_Reduce(mDotTotal, mDotTotalGlob, bufferSize, MPI_DOUBLE, MPI_SUM, 0, comm);
         MPI_Reduce(thrustTotal, thrustTotalGlob, bufferSize, MPI_DOUBLE, MPI_SUM, 0, comm);
+
+        // compile fields into vector [min max mean]
+        PetscReal pressureField[3] = {pressureMinGlob[0], pressureMaxGlob[0], pressureTotalGlob[0] / numCellsGlob[0]};
+        PetscReal machNumberField[3] = {machMinGlob[0], machMaxGlob[0], machTotalGlob[0] / numCellsGlob[0]};
 
         for (PetscInt d = 0; d < dim; d++) {
             if (tol < abs(thrustTotalGlob[d]) && 1e-2 < abs(mDotTotalGlob[d])) {  // avoid nan or dividing to near zero numbers
@@ -141,14 +198,20 @@ PetscErrorCode ablate::monitors::RocketMonitor::OutputRocket(TS ts, PetscInt ste
             }
         }
 
+        // print output
         if (rank == 0) {
             if (monitor->interval->Check(PetscObjectComm((PetscObject)ts), step, crtime)) {
                 if (monitor->name != "") {  // If user passed a name argument then output name
                     monitor->log->Printf("%s ", monitor->name.c_str());
                 }
                 monitor->log->Printf("RocketMonitor for timestep %04d: time: %-8.4g\n", (int)step, (double)crtime);
-                monitor->log->Printf("\tThrust:\t [ %1.7f, %1.7f, %1.7f]\n", thrustTotalGlob[0], thrustTotalGlob[1], thrustTotalGlob[2]);
-                monitor->log->Printf("\tIsp:\t [ %1.7f, %1.7f, %1.7f]\n", IspGlob[0], IspGlob[1], IspGlob[2]);
+                monitor->log->Printf("\tOutputs: [x y z]\n");
+                monitor->log->Printf("\tThrust:\t\t [ %1.7f, %1.7f, %1.7f ]\n", thrustTotalGlob[0], thrustTotalGlob[1], thrustTotalGlob[2]);
+                monitor->log->Printf("\tIsp:\t\t [ %1.7f, %1.7f, %1.7f ]\n", IspGlob[0], IspGlob[1], IspGlob[2]);
+                monitor->log->Printf("\tmDot:\t\t [ %1.7f, %1.7f, %1.7f ]\n", mDotTotalGlob[0], mDotTotalGlob[1], mDotTotalGlob[2]);
+                monitor->log->Printf("\tAdditional Fields: [min max mean]\n");
+                monitor->log->Printf("\tPressure:\t [ %1.7f, %1.7f, %1.7f ]\n", pressureField[0], pressureField[1], pressureField[2]);
+                monitor->log->Printf("\tmachNumber:\t [ %1.7f, %1.7f, %1.7f ]\n", machNumberField[0], machNumberField[1], machNumberField[2]);
             }
         }
 
