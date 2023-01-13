6b14eda13acdbed6c044553c78243e00dace8ff3
owenmylotte <owenmylotte@gmail.com>
2023-01-13 17:47:56 -0500

    Final radiation loss only process.

--- src/finiteVolume/processes/radiationLoss.cpp
+++ src/finiteVolume/processes/radiationLoss.cpp
@@ -1,11 +1,11 @@
 #include "radiationLoss.hpp"
-#include "finiteVolume/compressibleFlowFields.hpp"
 
-ablate::finiteVolume::processes::RadiationLoss::RadiationLoss(std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn) : radiationModel(std::move(radiationModelIn)) {}
+ablate::finiteVolume::processes::RadiationLoss::RadiationLoss(std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn, double tInfinityIn)
+    : radiationModel(std::move(radiationModelIn)), tInfinity(tInfinityIn == 0 ? 300 : tInfinityIn) {}
 
 void ablate::finiteVolume::processes::RadiationLoss::Setup(ablate::finiteVolume::FiniteVolumeSolver &fvmSolver) {
-        // add the source function
-        fvmSolver.RegisterRHSFunction(ComputeRadiationLoss, &absorptivityFunction, {ablate::finiteVolume::CompressibleFlowFields::EULER_FIELD}, {}, {ablate::finiteVolume::CompressibleFlowFields::TEMPERATURE_FIELD});
+    // add the source function
+    fvmSolver.RegisterRHSFunction(ComputeRadiationLoss, this, {ablate::finiteVolume::CompressibleFlowFields::EULER_FIELD}, {}, {ablate::finiteVolume::CompressibleFlowFields::TEMPERATURE_FIELD});
 
     absorptivityFunction = GetRadiationModel()->GetRadiationPropertiesTemperatureFunction(eos::radiationProperties::RadiationProperty::Absorptivity, fvmSolver.GetSubDomain().GetFields());
 }
@@ -13,24 +13,31 @@ void ablate::finiteVolume::processes::RadiationLoss::Setup(ablate::finiteVolume:
 PetscErrorCode ablate::finiteVolume::processes::RadiationLoss::ComputeRadiationLoss(PetscInt dim, PetscReal time, const PetscFVCellGeom *cg, const PetscInt *uOff, const PetscScalar *u,
                                                                                     const PetscInt *aOff, const PetscScalar *a, PetscScalar *f, void *ctx) {
     PetscFunctionBegin;
-    auto absorptivityFunction = (eos::ThermodynamicTemperatureFunction*)ctx;
-    auto absorptivityFunctionContext = absorptivityFunction->context.get();  //!< Get access to the absorption function
-    double kappa = 1;                  //!< Absorptivity coefficient, property of each cell
+    auto radiationLoss = (RadiationLoss *)ctx;
+    auto absorptivityFunction = radiationLoss->absorptivityFunction;
+    auto absorptivityFunctionContext = absorptivityFunction.context.get();  //!< Get access to the absorption function
+    double kappa = 1;                                                       //!< Absorptivity coefficient, property of each cell
 
     /**
      * Get the rhs values so that the absorption can be read out of the solution vector
      * Get the absorption out of the solution vector
      */
-    absorptivityFunction->function(u, a[0], &kappa, absorptivityFunctionContext); //! Temperature is the first offset in the aux array
+    absorptivityFunction.function(u, a[aOff[0]], &kappa, absorptivityFunctionContext);  //! Temperature is the first offset in the aux array
 
     /**
      * Add the computed intensity to the energy equation
      */
-    f[ablate::finiteVolume::CompressibleFlowFields::RHOE] += GetIntensity(a[0], kappa);  //!< Loop through the cells and update the equation of state
+    if ((GetIntensity(radiationLoss->tInfinity, a[aOff[0]], kappa) != 0) && (a[aOff[0]] < 300)) {
+        PetscPrintf(PETSC_COMM_WORLD, "%f %f %f\n", GetIntensity(radiationLoss->tInfinity, a[aOff[0]], kappa), a[aOff[0]], kappa);
+    }
+    for (int d = 0; d < dim; d++) f[ablate::finiteVolume::CompressibleFlowFields::RHOU + d] = 0;
+    f[ablate::finiteVolume::CompressibleFlowFields::RHO] = 0;
+    f[ablate::finiteVolume::CompressibleFlowFields::RHOE] = GetIntensity(radiationLoss->tInfinity, a[aOff[0]], kappa);  //!< Loop through the cells and update the equation of state
     PetscFunctionReturn(0);
 }
 
 #include "registrar.hpp"
-#define COMMA ,
-REGISTER_PASS_THROUGH(ablate::finiteVolume::processes::Process, ablate::finiteVolume::processes::RadiationLoss, "uses math functions to add arbitrary sources to the fvm method",
-                      ablate::eos::radiationProperties::RadiationModel);
+REGISTER_DEFAULT(ablate::finiteVolume::processes::Process, ablate::finiteVolume::processes::RadiationLoss,
+                 "Computes radiative losses for a volumetric region without evaluating the gains via raytracing",
+                 ARG(ablate::eos::radiationProperties::RadiationModel, "properties", "the radiation properties model"),
+                 OPT(double, "tInfinity", "far field temperature of the domain when computing radiation losses without ray tracing gains (default 300 K)"));
--- src/finiteVolume/processes/radiationLoss.hpp
+++ src/finiteVolume/processes/radiationLoss.hpp
@@ -4,6 +4,8 @@
 #include "eos/radiationProperties/radiationProperties.hpp"
 #include "process.hpp"
 #include "utilities/constants.hpp"
+#include "finiteVolume/compressibleFlowFields.hpp"
+
 namespace ablate::finiteVolume::processes {
 /**
  * This class uses math functions to add arbitrary sources to the fvm method
@@ -19,18 +21,8 @@ class RadiationLoss : public Process {
     static PetscErrorCode ComputeRadiationLoss(PetscInt dim, PetscReal time, const PetscFVCellGeom* cg, const PetscInt uOff[], const PetscScalar u[], const PetscInt aOff[], const PetscScalar a[],
                                                  PetscScalar f[], void* ctx);
 
-    //! pre store the petsc function and context
-    struct PetscFunctionStruct {
-        mathFunctions::PetscFunction petscFunction;
-        void* petscContext;
-        PetscInt fieldSize;
-    };
-
-    //! Store pointers to the petsc functions
-    std::vector<PetscFunctionStruct> petscFunctions;
-
    public:
-    explicit RadiationLoss(std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn);
+    explicit RadiationLoss(std::shared_ptr<eos::radiationProperties::RadiationModel> radiationModelIn, double tInfinityIn = 300);
 
     /**
      * public function to link this process with the fvm solver
@@ -46,14 +38,19 @@ class RadiationLoss : public Process {
      * @param kappa
      * @return
      */
-    static inline PetscReal GetIntensity(PetscReal temperature, PetscReal kappa) {
+    static inline PetscReal GetIntensity(PetscReal tInfinity, PetscReal temperature, PetscReal kappa) {
+
+        tInfinity = PetscMax(tInfinity, (temperature - 500));
+
         // Compute the losses
         PetscReal netIntensity = -4.0 * ablate::utilities::Constants::sbc * temperature * temperature * temperature * temperature;
+
+        netIntensity += 4.0 * ablate::utilities::Constants::sbc * tInfinity * tInfinity * tInfinity * tInfinity;
 
         // scale by kappa
         netIntensity *= kappa;
 
-        return abs(netIntensity) > ablate::utilities::Constants::large ? ablate::utilities::Constants::large * PetscSignReal(netIntensity) : netIntensity;
+        return PetscAbsReal(netIntensity) > ablate::utilities::Constants::large ? ablate::utilities::Constants::large * PetscSignReal(netIntensity) : netIntensity;
     }
 
     //! model used to provided the absorptivity function
@@ -61,6 +58,8 @@ class RadiationLoss : public Process {
 
     //! hold a pointer to the absorptivity function
     eos::ThermodynamicTemperatureFunction absorptivityFunction;
+
+    PetscReal tInfinity;
 };
 
 }  // namespace ablate::finiteVolume::processes
