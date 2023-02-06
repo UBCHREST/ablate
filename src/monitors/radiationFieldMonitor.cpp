cb64f9879a48d492926ea1e6dfa636c073cdddb7
owenmylotte <owenmylotte@gmail.com>
2023-02-06 11:02:43 -0500

    Runs and outputs zeros. Field information is not making it into the monitor?

--- src/monitors/radiationFieldMonitor.cpp
+++ src/monitors/radiationFieldMonitor.cpp
@@ -13,8 +13,7 @@ void ablate::monitors::RadiationFieldMonitor::Register(std::shared_ptr<ablate::s
     std::vector<std::shared_ptr<domain::FieldDescriptor>> fields(fieldNames.size(), nullptr);
 
     for (std::size_t f = 0; f < fieldNames.size(); f++) {
-        fields[f] =
-            std::make_shared<domain::FieldDescription>(fieldNames[f], fieldNames[f], domain::FieldDescription::ONECOMPONENT, domain::FieldLocation::SOL, domain::FieldType::FVM);
+        fields[f] = std::make_shared<domain::FieldDescription>(fieldNames[f], fieldNames[f], domain::FieldDescription::ONECOMPONENT, domain::FieldLocation::SOL, domain::FieldType::FVM);
     }
 
     // Register all fields with the monitorDomain
@@ -42,22 +41,15 @@ PetscErrorCode ablate::monitors::RadiationFieldMonitor::MonitorRadiation(TS ts, 
         // Increment the number of steps taken so far
         monitor->step += 1;
 
-        // Extract all fields to be monitored
-        std::vector<Vec> vec(monitor->fieldNames.size(), nullptr);
-        std::vector<IS> vecIS(monitor->fieldNames.size(), nullptr);
-        std::vector<DM> fieldDM(monitor->fieldNames.size(), nullptr);
-
-        for (std::size_t f = 0; f < monitor->fieldNames.size(); f++) {
-            const auto& field = monitor->GetSolver()->GetSubDomain().GetField(monitor->fieldNames[f]);
-            monitor->GetSolver()->GetSubDomain().GetFieldGlobalVector(field, &vecIS[f], &vec[f], &fieldDM[f]) >> utilities::PetscUtilities::checkError;
-        }
-
         // Extract the main solution vector for the absorptivity calculation
         DM solDM;
         Vec solVec;
         solVec = monitor->GetSolver()->GetSubDomain().GetSubSolutionVector();
         solDM = monitor->GetSolver()->GetSubDomain().GetDM();
 
+        /**
+         * We require the aux DM to extract temperature information.
+         */
         DM auxDm;
         Vec auxVec;
         auxVec = monitor->GetSolver()->GetSubDomain().GetAuxVector();
@@ -68,11 +60,19 @@ PetscErrorCode ablate::monitors::RadiationFieldMonitor::MonitorRadiation(TS ts, 
         Vec monitorVec = monitor->monitorSubDomain->GetSolutionVector();
         auto& monitorFields = monitor->monitorSubDomain->GetFields();
 
+        /**
+         * The monitor needs to read the information from the monitor DM to know which points it needs to write to
+         */
+        //        for (std::size_t f = 0; f < monitor->fieldNames.size(); f++) {
+        //            const auto& field = monitor->monitorSubDomain->GetField(monitor->fieldNames[f]);
+        //            monitor->GetSolver()->GetSubDomain().GetFieldGlobalVector(field, &vecIS[f], &vec[f], &fieldDM[f]) >> utilities::PetscUtilities::checkError;
+        //        }
+
         // Get the local cell range
         PetscInt cStart, cEnd;
         DMPlexGetHeightStratum(monitor->monitorSubDomain->GetSubDM(), 0, &cStart, &cEnd);
 
-        // Get the local to global cell mapping
+        //! Get the local to global cell mapping. This ensures that the cell index mapping between the monitor DM and the global solution is correct
         IS subpointIS;
         const PetscInt* subpointIndices;
         DMPlexGetSubpointIS(monitorDM, &subpointIS);
@@ -83,7 +83,7 @@ PetscErrorCode ablate::monitors::RadiationFieldMonitor::MonitorRadiation(TS ts, 
         VecGetArrayRead(solVec, &solDat) >> utilities::PetscUtilities::checkError;
 
         // Extract the aux global array
-        const PetscScalar *auxDat;
+        const PetscScalar* auxDat;
         VecGetArrayRead(auxVec, &auxDat) >> utilities::PetscUtilities::checkError;
 
         // Extract the monitor array
@@ -100,21 +100,25 @@ PetscErrorCode ablate::monitors::RadiationFieldMonitor::MonitorRadiation(TS ts, 
         const PetscScalar* fieldDat;
         double kappa = 1;                  //!< Absorptivity coefficient, property of each cell
         PetscReal* temperature = nullptr;  //!< The temperature at any given location
-        const PetscScalar* fieldPt;
+//        const PetscScalar* fieldPt;
         const PetscScalar* solPt;
+
         PetscScalar* monitorPt;
 
         for (std::size_t f = 0; f < monitor->fieldNames.size(); f++) {
             // Extract the field vector global array
-            VecGetArrayRead(vec[f], &fieldDat) >> utilities::PetscUtilities::checkError;
+            VecGetArrayRead(monitorVec, &fieldDat) >> utilities::PetscUtilities::checkError;  // TODO: Do we need an offset here?
 
             //! Compute measures
             for (PetscInt c = cStart; c < cEnd; c++) {
                 PetscInt monitorCell = c;
-                PetscInt masterCell = subpointIndices[monitorCell];
+                PetscInt masterCell = subpointIndices[monitorCell];  //! Gets the cell index associated with this position in the monitor DM cell range?
 
-                // Get field point data
-                DMPlexPointLocalRead(fieldDM[f], masterCell, fieldDat, &fieldPt) >> utilities::PetscUtilities::checkError;
+                // TODO: We don't need to extract any information from the monitor field to get the new values.
+
+                //                // Get field point data
+                //                DMPlexPointLocalFieldRead(monitorDM, cellSegment.cell, temperatureField.id, auxArray, &temperature);
+                //                DMPlexPointLocalRead(fieldDM[f], masterCell, fieldDat, &fieldPt) >> utilities::PetscUtilities::checkError;
 
                 // Get solution point data
                 DMPlexPointLocalRead(solDM, masterCell, solDat, &solPt) >> utilities::PetscUtilities::checkError;
@@ -123,9 +127,8 @@ PetscErrorCode ablate::monitors::RadiationFieldMonitor::MonitorRadiation(TS ts, 
                 DMPlexPointGlobalRef(monitorDM, monitorCell, monitorDat, &monitorPt) >> utilities::PetscUtilities::checkError;
 
                 if (monitorPt && solPt) {
-
                     // compute absorptivity
-                    DMPlexPointLocalFieldRead(auxDm, monitorCell, temperatureFieldInfo.id, auxDat, &temperature) >> utilities::PetscUtilities::checkError;
+                    DMPlexPointLocalFieldRead(auxDm, masterCell, temperatureFieldInfo.id, auxDat, &temperature) >> utilities::PetscUtilities::checkError;
 
                     // Get the absorptivity data from solution point data
                     monitor->absorptivityFunction.function(solPt, *temperature, &kappa, monitor->absorptivityFunction.context.get());
@@ -133,28 +136,26 @@ PetscErrorCode ablate::monitors::RadiationFieldMonitor::MonitorRadiation(TS ts, 
                     // Perform actual calculations now
                     monitorPt[monitorFields[FieldPlacements::intensity].offset] += kappa * ablate::utilities::Constants::sbc * *temperature * *temperature * *temperature * *temperature;
                     monitorPt[monitorFields[FieldPlacements::absorption].offset] += kappa;
-
                 }
             }
-            VecRestoreArrayRead(vec[f], &fieldDat) >> utilities::PetscUtilities::checkError;
+            VecRestoreArrayRead(monitorVec, &fieldDat) >> utilities::PetscUtilities::checkError;  // TODO: Do we need an offset here?
         }
         // Cleanup
         // Restore arrays
         ISRestoreIndices(subpointIS, &subpointIndices) >> utilities::PetscUtilities::checkError;
         VecRestoreArrayRead(solVec, &solDat) >> utilities::PetscUtilities::checkError;
         VecRestoreArray(monitorVec, &monitorDat) >> utilities::PetscUtilities::checkError;
-        // Restore field vectors
-        for (std::size_t f = 0; f < monitor->fieldNames.size(); f++) {
-            const auto& field = monitor->GetSolver()->GetSubDomain().GetField(monitor->fieldNames[f]);
-            monitor->GetSolver()->GetSubDomain().RestoreFieldGlobalVector(field, &vecIS[f], &vec[f], &fieldDM[f]) >> utilities::PetscUtilities::checkError;
-        }
+        //        // Restore field vectors
+        //        for (std::size_t f = 0; f < monitor->fieldNames.size(); f++) {
+        //            const auto& field = monitor->GetSolver()->GetSubDomain().GetField(monitor->fieldNames[f]);
+        //            monitor->GetSolver()->GetSubDomain().RestoreFieldGlobalVector(field, &vecIS[f], &vec[f], &fieldDM[f]) >> utilities::PetscUtilities::checkError;
+        //        }
     }
 
     PetscFunctionReturn(0);
 }
 
 #include "registrar.hpp"
-REGISTER(ablate::monitors::Monitor, ablate::monitors::RadiationFieldMonitor, "A solver for radiative heat transfer in participating media",
-         ARG(ablate::eos::EOS, "eos", "The equation of state"),
+REGISTER(ablate::monitors::Monitor, ablate::monitors::RadiationFieldMonitor, "A solver for radiative heat transfer in participating media", ARG(ablate::eos::EOS, "eos", "The equation of state"),
          ARG(ablate::eos::radiationProperties::RadiationModel, "properties", "properties model for the output of radiation properties within the field"),
          OPT(ablate::io::interval::Interval, "interval", "The monitor output interval"));
