9e438c24b23b5594a398fd454a4b3f972a483e76
David Salac <davidsal@buffalo.edu>
2023-03-20 09:21:07 -0400

    Reverted main.cpp

--- main.cpp
+++ main.cpp
@@ -1,142 +1,111 @@
-#include <petsc.h>
+#include <fstream>
+#include <iostream>
 #include <memory>
-#include "domain/RBF/ga.hpp"
-#include "domain/RBF/hybrid.hpp"
-#include "domain/RBF/imq.hpp"
-#include "domain/RBF/mq.hpp"
-#include "domain/RBF/phs.hpp"
-#include "domain/RBF/rbf.hpp"
-#include "domain/boxMesh.hpp"
-#include "domain/modifiers/distributeWithGhostCells.hpp"
+#include "builder.hpp"
+#include "environment/download.hpp"
 #include "environment/runEnvironment.hpp"
+#include "listing.hpp"
+#include "localPath.hpp"
+#include "parameters/petscPrefixOptions.hpp"
+#include "utilities/mpiUtilities.hpp"
 #include "utilities/petscUtilities.hpp"
-
-
+#include "yamlParser.hpp"
 
 using namespace ablate;
 
+const char* replacementInputPrefix = "-yaml::";
 
-static PetscReal RBFTestFixture_Function(PetscReal x[3], PetscInt dx, PetscInt dy, PetscInt dz) {
-    switch (dx + 10 * dy + 100 * dz) {
-        case 0:  // Function
-            return (1 + sin(4.0 * x[0]) + sin(4.0 * x[1]) + sin(4.0 * x[2]) + cos(x[0] + x[1] + x[2]));
-        case 1:  // x
-            return (4.0 * cos(4.0 * x[0]) - sin(x[0] + x[1] + x[2]));
-        case 2:  // xx
-            return (-cos(x[0] + x[1] + x[2]) - 16.0 * sin(4.0 * x[0]));
-        case 10:  // y
-            return (4.0 * cos(4.0 * x[1]) - sin(x[0] + x[1] + x[2]));
-        case 11:  // xy
-            return (-cos(x[0] + x[1] + x[2]));
-        case 20:  // yy
-            return (-cos(x[0] + x[1] + x[2]) - 16.0 * sin(4.0 * x[1]));
-        case 100:  // z
-            return (4.0 * cos(4.0 * x[2]) - sin(x[0] + x[1] + x[2]));
-        case 101:  // xz
-            return (-cos(x[0] + x[1] + x[2]));
-        case 110:  // yz
-            return (-cos(x[0] + x[1] + x[2]));
-        case 200:  // zz
-            return (-cos(x[0] + x[1] + x[2]) - 16.0 * sin(4.0 * x[2]));
-        default:
-            throw std::invalid_argument("Unknown derivative of (" + std::to_string(dx) + ", " + std::to_string(dy) + ", " + std::to_string(dz) + ").");
+int main(int argc, char** args) {
+    // initialize petsc and mpi
+    environment::RunEnvironment::Initialize(&argc, &args);
+    ablate::utilities::PetscUtilities::Initialize();
+
+    // check to see if we should print version
+    PetscBool printInfo = PETSC_FALSE;
+    PetscOptionsGetBool(nullptr, nullptr, "-version", &printInfo, nullptr) >> utilities::PetscUtilities::checkError;
+    if (!printInfo) {
+        PetscOptionsGetBool(nullptr, nullptr, "--info", &printInfo, nullptr) >> utilities::PetscUtilities::checkError;
     }
-}
-
-void RBFTestFixture_SetData(ablate::solver::Range cellRange, const ablate::domain::Field *field, std::shared_ptr<ablate::domain::SubDomain> subDomain) {
-    PetscReal *array, *val, x[3] = {0.0, 0.0, 0.0};
-    Vec vec = subDomain->GetVec(*field);
-    DM dm = subDomain->GetFieldDM(*field);
-
-    VecGetArray(vec, &array) >> utilities::PetscUtilities::checkError;
-    for (PetscInt c = cellRange.start; c < cellRange.end; ++c) {
-        PetscInt cell = cellRange.points ? cellRange.points[c] : c;
-        DMPlexComputeCellGeometryFVM(dm, cell, NULL, x, NULL) >> utilities::PetscUtilities::checkError;
-        DMPlexPointLocalFieldRef(dm, cell, field->id, array, &val) >> utilities::PetscUtilities::checkError;
-        *val = RBFTestFixture_Function(x, 0, 0, 0);
-    }
-    VecRestoreArray(vec, &array) >> utilities::PetscUtilities::checkError;
-}
-
-
-int main(int argc, char** argv) {
-
-
-
-  environment::RunEnvironment::Initialize(&argc, &argv);
-  utilities::PetscUtilities::Initialize();
-
-  std::vector<std::shared_ptr<domain::rbf::RBF>> rbfList = {std::make_shared<ablate::domain::rbf::GA>(4, 3.888078956798655e-02, false, false),
-                                                            std::make_shared<ablate::domain::rbf::MQ>(4, 3.888078956798655e-02, false, false),
-                                                            std::make_shared<ablate::domain::rbf::IMQ>(4, 3.888078956798655e-02, false, false),
-                                                            std::make_shared<ablate::domain::rbf::PHS>(4, 2, false, false),
-                                                            std::make_shared<ablate::domain::rbf::HYBRID>(4, std::vector<double>{1.0, 0.001},
-                                                                                                          std::vector<std::shared_ptr<ablate::domain::rbf::RBF>>{
-                                                                                                              std::make_shared<ablate::domain::rbf::GA>(4, 3.888078956798655e-02, false, false),
-                                                                                                              std::make_shared<ablate::domain::rbf::PHS>(4, 2, false, false)},
-                                                                                                          false, false)};
-
-  //             Make the field
-  std::vector<std::shared_ptr<ablate::domain::FieldDescriptor>> fieldDescriptor = {
-      std::make_shared<ablate::domain::FieldDescription>("fieldA", "", ablate::domain::FieldDescription::ONECOMPONENT, ablate::domain::FieldLocation::AUX, ablate::domain::FieldType::FVM)};
-
-  //             Create the mesh
-  //      Note that using -dm_view :mesh.tex:ascii_latex -dm_plex_view_scale 10 -dm_plex_view_numbers_depth 1,0,1 will create a mesh, changing numbers_depth as appropriate
-  auto mesh = std::make_shared<domain::BoxMesh>("mesh", fieldDescriptor, std::vector<std::shared_ptr<domain::modifiers::Modifier>>{std::make_shared<domain::modifiers::DistributeWithGhostCells>(3)},
-                                                std::vector<int>{21, 21, 21}, std::vector<double>{-1.0, -1.0, -1.0}, std::vector<double>{1.0 ,1.0, 1.0}, std::vector<std::string>{}, true);
-
-  mesh->InitializeSubDomains();
-
-  std::shared_ptr<ablate::domain::SubDomain> subDomain = mesh->GetSubDomain(domain::Region::ENTIREDOMAIN);
-
-  // The field containing the data
-  const ablate::domain::Field *field = &(subDomain->GetField("fieldA"));
-
-  ablate::solver::Range cellRange;
-  for (unsigned long int j = 0; j < rbfList.size(); ++j) {
-      rbfList[j]->Setup(subDomain);  // This causes issues (I think)
-
-      //         Initialize
-      rbfList[j]->GetCellRange(subDomain, nullptr, cellRange);
-      rbfList[j]->Initialize(cellRange);
-      rbfList[j]->RestoreRange(cellRange);
-  }
-
-  // Now set the data using the first RBF. All will use the same data
-  rbfList[0]->GetCellRange(subDomain, nullptr, cellRange);
-  RBFTestFixture_SetData(cellRange, field, subDomain);
-
-  // Now check derivatives
-  std::vector<PetscInt> dx = {0, 1, 2, 0, 1, 0, 0, 1, 0, 0}, dy = {0, 0, 0, 1, 1, 2, 0, 0, 1, 0}, dz = {0, 0, 0, 0, 0, 0, 1, 1, 1, 2};
-//  std::vector<PetscReal> maxErrorList = {1e-15, 3.5e-05, 3.5e-03, 5.6e-05, 1.7e-04, 3.0e-03, 1.51e-04, 6.3e-03, 5.9e-03, 2.1e-03};
-  PetscInt c, cell;
-//  PetscReal maxError;
-  PetscReal x[3];
-  PetscReal err = -1.0;
-  PetscReal val;
-  DM dm = subDomain->GetDM();
-
-  for (unsigned long int i = 0; i < dx.size(); ++i) {  // Iterate over each of the requested derivatives
-
-        // 3D results take too long to run, so just check a corner
-        for (unsigned long int j = 0; j < rbfList.size(); ++j) {  // Check each RBF
-            c = 0;
-
-            cell = cellRange.points ? cellRange.points[c] : c;
-            val = rbfList[j]->EvalDer(field, c, dx[i], dy[i], dz[i]);
-
-            DMPlexComputeCellGeometryFVM(dm, cell, NULL, x, NULL) >> ablate::utilities::PetscUtilities::checkError;
-            err = PetscAbsReal(val - RBFTestFixture_Function(x, dx[i], dy[i], dz[i]));
-            printf("%e\n", err);
-
-      }
-
+    if (printInfo) {
+        Builder::PrintInfo(std::cout);
+        std::cout << "----------------------------------------" << std::endl;
     }
 
+    PetscBool printVersion = PETSC_FALSE;
+    PetscOptionsGetBool(nullptr, nullptr, "--version", &printVersion, nullptr) >> utilities::PetscUtilities::checkError;
+    if (printVersion) {
+        Builder::PrintVersion(std::cout);
+        return 0;
+    }
 
-  rbfList[0]->RestoreRange(cellRange);
+    PetscInt delay = -1;
+    PetscBool delaySpecified;
+    PetscOptionsGetInt(nullptr, nullptr, "--delay", &delay, &delaySpecified) >> utilities::PetscUtilities::checkError;
+    if (delaySpecified) {
+        PetscSleep(delay) >> utilities::PetscUtilities::checkError;
+    }
 
-  //      ablate::environment::RunEnvironment::Finalize();
+    // check to see if we should print options
+    PetscBool printParserOptions = PETSC_FALSE;
+    PetscOptionsGetBool(nullptr, nullptr, "--help", &printParserOptions, nullptr) >> utilities::PetscUtilities::checkError;
+    if (printParserOptions) {
+        std::cout << cppParser::Listing::Get() << std::endl;
+        return 0;
+    }
 
+    // check to see if we should print options
+    char filename[PETSC_MAX_PATH_LEN] = "";
+    PetscBool fileSpecified = PETSC_FALSE;
+    PetscOptionsGetString(nullptr, nullptr, "--input", filename, PETSC_MAX_PATH_LEN, &fileSpecified) >> utilities::PetscUtilities::checkError;
+    if (!fileSpecified) {
+        throw std::invalid_argument("the --input must be specified");
+    }
 
+    // locate or download the file
+    std::filesystem::path filePath;
+    if (ablate::environment::Download::IsUrl(filename)) {
+        ablate::environment::Download downloader(filename);
+        filePath = downloader.Locate();
+    } else {
+        cppParser::LocalPath locator(filename);
+        filePath = locator.Locate();
+    }
+
+    if (!std::filesystem::exists(filePath)) {
+        throw std::invalid_argument("unable to locate input file: " + filePath.string());
+    }
+    {
+        // build options from the command line
+        auto yamlOptions = std::make_shared<ablate::parameters::PetscPrefixOptions>(replacementInputPrefix);
+
+        // create the yaml parser
+        std::shared_ptr<cppParser::YamlParser> parser = std::make_shared<cppParser::YamlParser>(filePath, yamlOptions->GetMap());
+
+        // setup the monitor
+        auto setupEnvironmentParameters = parser->GetByName<ablate::parameters::Parameters>("environment");
+        environment::RunEnvironment::Setup(*setupEnvironmentParameters, filePath);
+
+        // Copy over the input file
+        int rank;
+        MPI_Comm_rank(PETSC_COMM_WORLD, &rank) >> utilities::MpiUtilities::checkError;
+        if (rank == 0) {
+            std::filesystem::path inputCopy = environment::RunEnvironment::Get().GetOutputDirectory() / filePath.filename();
+            std::ofstream stream(inputCopy);
+            parser->Print(stream);
+            stream.close();
+        }
+
+        // run with the parser
+        Builder::Run(parser);
+
+        // check for unused parameters
+        auto unusedValues = parser->GetUnusedValues();
+        if (!unusedValues.empty()) {
+            std::cout << "WARNING: The following input parameters were not used:" << std::endl;
+            for (auto unusedValue : unusedValues) {
+                std::cout << unusedValue << std::endl;
+            }
+        }
+    }
+    ablate::environment::RunEnvironment::Finalize();
 }
