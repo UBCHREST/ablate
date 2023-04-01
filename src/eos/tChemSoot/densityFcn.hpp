4faa3131e87207b0287c84760cc62864427053fc
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-01 15:59:52 -0400

    debug compile

--- src/eos/tChemSoot/densityFcn.hpp
+++ src/eos/tChemSoot/densityFcn.hpp
@@ -20,21 +20,22 @@ struct densityFcn {
     KOKKOS_INLINE_FUNCTION static value_type team_invoke(const MemberType& member,
                                                          /// input
                                                          StateVectorSoot<real_type_1d_view_type> totalState, const KineticModelConstDataType& kmcd) {
-        member.team_barrier();
-        real_type_1d_view gaseousState = real_type_1d_view_type("Gaseous", ::TChem::Impl::getStateVectorSize(kmcd.nSpec));
-        ::TChem::Impl::StateVector svGas = ::TChem::Impl::StateVector(kmcd.nSpec, gaseousState);
-
-        totalState.SplitYiState(svGas);
-
-        value_type totalDensity;
-        auto YCarbon = totalState.MassFractionCarbon();
-        TCHEM_CHECK_ERROR(!svGas.isValid(), "Error: input state vector is not valid");
-        {
-            value_type temperature = svGas.Temperature();
-            value_type gaseousDensity = Impl::RhoMixMs<value_type, DeviceType>::team_invoke(member, temperature, svGas.Pressure(), svGas.MassFractions(), kmcd);
-            totalDensity = 1.0 / ((1.0 - YCarbon) / gaseousDensity + (YCarbon / ablate::eos::tChemSoot::solidCarbonDensity));
-        }
-        return totalDensity;
+//        member.team_barrier();
+//        real_type_1d_view gaseousState = real_type_1d_view_type("Gaseous", ::TChem::Impl::getStateVectorSize(kmcd.nSpec));
+//        ::TChem::Impl::StateVector svGas = ::TChem::Impl::StateVector(kmcd.nSpec, gaseousState);
+//
+//        totalState.SplitYiState(svGas);
+//
+//        value_type totalDensity;
+//        auto YCarbon = totalState.MassFractionCarbon();
+//        TCHEM_CHECK_ERROR(!svGas.isValid(), "Error: input state vector is not valid");
+//        {
+//            value_type temperature = svGas.Temperature();
+//            value_type gaseousDensity = Impl::RhoMixMs<value_type, DeviceType>::team_invoke(member, temperature, svGas.Pressure(), svGas.MassFractions(), kmcd);
+//            totalDensity = 1.0 / ((1.0 - YCarbon) / gaseousDensity + (YCarbon / ablate::eos::tChemSoot::solidCarbonDensity));
+//        }
+//        return totalDensity;
+        return 0.0;
     }
 };
 
