56b38ee9c589ff78cbea7abd9cd4127d7f17909f
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-01 13:52:04 -0400

    switched to host

--- src/eos/tChem.cpp
+++ src/eos/tChem.cpp
@@ -711,7 +711,7 @@ ablate::eos::EOSFunction ablate::eos::TChem::GetFieldFunctionFunction(const std:
                         auto cpks = real_type_1d_view_host((real_type *)work.data(), per_team_extent);
 
                         auto sensibleInternalEnergy = ablate::eos::tChem::impl::SensibleInternalEnergyFcn<real_type, host_device_type>::team_invoke(
-                            member, temperature, yiHost, enthalpy, cpks, enthalpyReferenceDevice, kineticsModelDataHost);
+                            member, temperature, yiHost, enthalpy, cpks, enthalpyReferenceHost, kineticsModelDataHost);
 
                         // convert to total sensibleEnergy
                         PetscReal kineticEnergy = 0;
@@ -774,7 +774,7 @@ ablate::eos::EOSFunction ablate::eos::TChem::GetFieldFunctionFunction(const std:
                 double R = kineticsModelDataHost.Runiv / mwMix(0);
 
                 // compute the temperature
-                eos::tChem::Temperature::runHostBatch(policy, stateHostView, internalEnergy, enthalpy, enthalpyReferenceDevice, kineticsModelDataHost);
+                eos::tChem::Temperature::runHostBatch(policy, stateHostView, internalEnergy, enthalpy, enthalpyReferenceHost, kineticsModelDataHost);
 
                 // compute pressure p = rho*R*T
                 PetscReal density = pressure / (stateHost.Temperature() * R);
