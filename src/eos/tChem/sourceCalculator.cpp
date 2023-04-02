d9578332163e4aa337b797096a877a3389c735aa
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-02 16:58:15 -0400

    debug

--- src/eos/tChem/sourceCalculator.cpp
+++ src/eos/tChem/sourceCalculator.cpp
@@ -211,8 +211,9 @@ void ablate::eos::tChem::SourceCalculator::ComputeSource(SourceCalculator& sourc
         // Use a parallel for updating timeAdvanceDevice dt
         Kokkos::parallel_for(
             "timeAdvanceUpdate", Kokkos::RangePolicy<tChemLib::exec_space>(0, numberCells), KOKKOS_LAMBDA(const ordinal_type& i) {
-                  printf("%d", i);
-//                auto& tAdvAtI = timeAdvanceDeviceLocal(i);
+                auto& tAdvAtI = timeAdvanceDeviceLocal(i);
+                printf("%f", tAdvAtI._dt);
+
 //                tAdvAtI._tbeg = time;
 //                tAdvAtI._tend = time + dt;
 ////                tAdvAtI._dt = PetscMax(PetscMin(PetscMin(dtViewDeviceLocal(i) * chemistryConstraintsLocal.dtEstimateFactor, dt), tAdvAtI._dtmax) / factor, tAdvAtI._dtmin);
