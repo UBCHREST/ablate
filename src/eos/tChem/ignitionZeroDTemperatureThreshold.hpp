e8cc3d4f9a5293e9a287aaf4da454e75be147980
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-02 13:09:05 -0400

    removed soot

--- src/eos/tChem/ignitionZeroDTemperatureThreshold.hpp
+++ src/eos/tChem/ignitionZeroDTemperatureThreshold.hpp
@@ -28,130 +28,127 @@ Sandia National Laboratories, Livermore, CA, USA
 
 namespace ablate::eos::tChem {
 
-struct IgnitionZeroDTemperatureThreshold {
-   public:
-    template <typename PolicyType, typename ValueType,
-              typename DeviceType>
-    static void IgnitionZeroDTemperatureThreshold_TemplateRun(  /// required template arguments
-        const std::string& profile_name, const ValueType& dummyValueType,
-        /// team size setting
-        const PolicyType& policy,
+template <typename PolicyType, typename ValueType,
+          typename DeviceType>
+void IgnitionZeroDTemperatureThreshold_TemplateRun(  /// required template arguments
+    const std::string& profile_name, const ValueType& dummyValueType,
+    /// team size setting
+    const PolicyType& policy,
 
-        /// input
-        const Tines::value_type_1d_view<real_type, DeviceType>& tol_newton, const Tines::value_type_2d_view<real_type, DeviceType>& tol_time,
-        const Tines::value_type_2d_view<real_type, DeviceType>& fac, const Tines::value_type_1d_view<time_advance_type, DeviceType>& tadv,
-        const Tines::value_type_2d_view<real_type, DeviceType>& state,
-        /// output
-        const Tines::value_type_1d_view<real_type, DeviceType>& t_out, const Tines::value_type_1d_view<real_type, DeviceType>& dt_out,
-        const Tines::value_type_2d_view<real_type, DeviceType>& state_out,
-        /// const data from kinetic model
-        const Tines::value_type_1d_view<KineticModelConstData<DeviceType>, DeviceType>& kmcds, double thresholdTemperature) {
-        Kokkos::Profiling::pushRegion(profile_name);
-        using policy_type = PolicyType;
+    /// input
+    const Tines::value_type_1d_view<real_type, DeviceType>& tol_newton, const Tines::value_type_2d_view<real_type, DeviceType>& tol_time, const Tines::value_type_2d_view<real_type, DeviceType>& fac,
+    const Tines::value_type_1d_view<time_advance_type, DeviceType>& tadv, const Tines::value_type_2d_view<real_type, DeviceType>& state,
+    /// output
+    const Tines::value_type_1d_view<real_type, DeviceType>& t_out, const Tines::value_type_1d_view<real_type, DeviceType>& dt_out, const Tines::value_type_2d_view<real_type, DeviceType>& state_out,
+    /// const data from kinetic model
+    const Tines::value_type_1d_view<KineticModelConstData<DeviceType>, DeviceType>& kmcds, double thresholdTemperature) {
+    Kokkos::Profiling::pushRegion(profile_name);
+    using policy_type = PolicyType;
 
-        using RealType0DViewType = Tines::value_type_0d_view<real_type, DeviceType>;
-        using RealType1DViewType = Tines::value_type_1d_view<real_type, DeviceType>;
+    using RealType0DViewType = Tines::value_type_0d_view<real_type, DeviceType>;
+    using RealType1DViewType = Tines::value_type_1d_view<real_type, DeviceType>;
 
-        auto kmcd_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), Kokkos::subview(kmcds, 0));
+    auto kmcd_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), Kokkos::subview(kmcds, 0));
 
-        const ordinal_type level = 1;
-        const ordinal_type per_team_extent = IgnitionZeroD::getWorkSpaceSize(kmcd_host());
+    const ordinal_type level = 1;
+    const ordinal_type per_team_extent = IgnitionZeroD::getWorkSpaceSize(kmcd_host());
 
-        Kokkos::parallel_for(
-            profile_name, policy, KOKKOS_LAMBDA(const typename policy_type::member_type& member) {
-                const ordinal_type i = member.league_rank();
-                const auto kmcd_at_i = (kmcds.extent(0) == 1 ? kmcds(0) : kmcds(i));
-                const auto tadv_at_i = tadv(i);
-                const real_type t_end = tadv_at_i._tend;
-                const RealType0DViewType t_out_at_i = Kokkos::subview(t_out, i);
-                if (t_out_at_i() < t_end) {
-                    const RealType1DViewType state_at_i = Kokkos::subview(state, i, Kokkos::ALL());
-                    const RealType1DViewType state_out_at_i = Kokkos::subview(state_out, i, Kokkos::ALL());
-                    const RealType1DViewType fac_at_i = Kokkos::subview(fac, i, Kokkos::ALL());
+    Kokkos::parallel_for(
+        profile_name, policy, KOKKOS_LAMBDA(const typename policy_type::member_type& member) {
+            const ordinal_type i = member.league_rank();
+            const auto kmcd_at_i = (kmcds.extent(0) == 1 ? kmcds(0) : kmcds(i));
+            const auto tadv_at_i = tadv(i);
+            const real_type t_end = tadv_at_i._tend;
+            const RealType0DViewType t_out_at_i = Kokkos::subview(t_out, i);
+            if (t_out_at_i() < t_end) {
+                const RealType1DViewType state_at_i = Kokkos::subview(state, i, Kokkos::ALL());
+                const RealType1DViewType state_out_at_i = Kokkos::subview(state_out, i, Kokkos::ALL());
+                const RealType1DViewType fac_at_i = Kokkos::subview(fac, i, Kokkos::ALL());
 
-                    const RealType0DViewType dt_out_at_i = Kokkos::subview(dt_out, i);
-                    Scratch<RealType1DViewType> work(member.team_scratch(level), per_team_extent);
+                const RealType0DViewType dt_out_at_i = Kokkos::subview(dt_out, i);
+                Scratch<RealType1DViewType> work(member.team_scratch(level), per_team_extent);
 
-                    Impl::StateVector<RealType1DViewType> sv_at_i(kmcd_at_i.nSpec, state_at_i);
-                    Impl::StateVector<RealType1DViewType> sv_out_at_i(kmcd_at_i.nSpec, state_out_at_i);
-                    TCHEM_CHECK_ERROR(!sv_at_i.isValid(), "Error: input state vector is not valid");
-                    TCHEM_CHECK_ERROR(!sv_out_at_i.isValid(), "Error: input state vector is not valid");
-                    {
-                        const ordinal_type jacobian_interval = tadv_at_i._jacobian_interval;
-                        const ordinal_type max_num_newton_iterations = tadv_at_i._max_num_newton_iterations;
-                        const ordinal_type max_num_time_iterations = tadv_at_i._num_time_iterations_per_interval;
+                Impl::StateVector<RealType1DViewType> sv_at_i(kmcd_at_i.nSpec, state_at_i);
+                Impl::StateVector<RealType1DViewType> sv_out_at_i(kmcd_at_i.nSpec, state_out_at_i);
+                TCHEM_CHECK_ERROR(!sv_at_i.isValid(), "Error: input state vector is not valid");
+                TCHEM_CHECK_ERROR(!sv_out_at_i.isValid(), "Error: input state vector is not valid");
+                {
+                    const ordinal_type jacobian_interval = tadv_at_i._jacobian_interval;
+                    const ordinal_type max_num_newton_iterations = tadv_at_i._max_num_newton_iterations;
+                    const ordinal_type max_num_time_iterations = tadv_at_i._num_time_iterations_per_interval;
 
-                        const real_type dt_in = tadv_at_i._dt, dt_min = tadv_at_i._dtmin, dt_max = tadv_at_i._dtmax;
-                        const real_type t_beg = tadv_at_i._tbeg;
+                    const real_type dt_in = tadv_at_i._dt, dt_min = tadv_at_i._dtmin, dt_max = tadv_at_i._dtmax;
+                    const real_type t_beg = tadv_at_i._tbeg;
 
-                        const auto temperature = sv_at_i.Temperature();
+                    const auto temperature = sv_at_i.Temperature();
 
-                        if (temperature > thresholdTemperature) {
-                            const auto pressure = sv_at_i.Pressure();
-                            const auto Ys = sv_at_i.MassFractions();
+                    if (temperature > thresholdTemperature) {
+                        const auto pressure = sv_at_i.Pressure();
+                        const auto Ys = sv_at_i.MassFractions();
 
-                            const RealType0DViewType temperature_out(sv_out_at_i.TemperaturePtr());
-                            const RealType0DViewType pressure_out(sv_out_at_i.PressurePtr());
-                            const RealType1DViewType Ys_out = sv_out_at_i.MassFractions();
-                            const RealType0DViewType density_out(sv_out_at_i.DensityPtr());
+                        const RealType0DViewType temperature_out(sv_out_at_i.TemperaturePtr());
+                        const RealType0DViewType pressure_out(sv_out_at_i.PressurePtr());
+                        const RealType1DViewType Ys_out = sv_out_at_i.MassFractions();
+                        const RealType0DViewType density_out(sv_out_at_i.DensityPtr());
 
-                            const ordinal_type m = kmcd_at_i.nSpec + 1;
-                            auto wptr = work.data();
-                            const RealType1DViewType vals(wptr, m);
-                            wptr += m;
-                            const RealType1DViewType ww(wptr, work.extent(0) - (wptr - work.data()));
+                        const ordinal_type m = kmcd_at_i.nSpec + 1;
+                        auto wptr = work.data();
+                        const RealType1DViewType vals(wptr, m);
+                        wptr += m;
+                        const RealType1DViewType ww(wptr, work.extent(0) - (wptr - work.data()));
 
-                            /// we can only guarantee vals is contiguous array. we basically assume
-                            /// that a state vector can be arbitrary ordered.
+                        /// we can only guarantee vals is contiguous array. we basically assume
+                        /// that a state vector can be arbitrary ordered.
 
-                            /// m is nSpec + 1
-                            Kokkos::parallel_for(Kokkos::TeamVectorRange(member, m), [&](const ordinal_type& i) { vals(i) = i == 0 ? temperature : Ys(i - 1); });
-                            member.team_barrier();
+                        /// m is nSpec + 1
+                        Kokkos::parallel_for(Kokkos::TeamVectorRange(member, m), [&](const ordinal_type& i) { vals(i) = i == 0 ? temperature : Ys(i - 1); });
+                        member.team_barrier();
 
-                            using ignition_zeroD = Impl::IgnitionZeroD<ValueType, DeviceType>;
+                        using ignition_zeroD = Impl::IgnitionZeroD<ValueType, DeviceType>;
 
-                            ignition_zeroD ::team_invoke(member,
-                                                         jacobian_interval,
-                                                         max_num_newton_iterations,
-                                                         max_num_time_iterations,
-                                                         tol_newton,
-                                                         tol_time,
-                                                         fac_at_i,
-                                                         dt_in,
-                                                         dt_min,
-                                                         dt_max,
-                                                         t_beg,
-                                                         t_end,
-                                                         pressure,
-                                                         vals,
-                                                         t_out_at_i,
-                                                         dt_out_at_i,
-                                                         pressure_out,
-                                                         vals,
-                                                         ww,
-                                                         kmcd_at_i);
+                        ignition_zeroD ::team_invoke(member,
+                                                     jacobian_interval,
+                                                     max_num_newton_iterations,
+                                                     max_num_time_iterations,
+                                                     tol_newton,
+                                                     tol_time,
+                                                     fac_at_i,
+                                                     dt_in,
+                                                     dt_min,
+                                                     dt_max,
+                                                     t_beg,
+                                                     t_end,
+                                                     pressure,
+                                                     vals,
+                                                     t_out_at_i,
+                                                     dt_out_at_i,
+                                                     pressure_out,
+                                                     vals,
+                                                     ww,
+                                                     kmcd_at_i);
 
-                            member.team_barrier();
-                            Kokkos::parallel_for(Kokkos::TeamVectorRange(member, m), [&](const ordinal_type& i) {
-                                if (i == 0) {
-                                    temperature_out() = vals(0);
-                                } else {
-                                    Ys_out(i - 1) = vals(i);
-                                }
-                            });
+                        member.team_barrier();
+                        Kokkos::parallel_for(Kokkos::TeamVectorRange(member, m), [&](const ordinal_type& i) {
+                            if (i == 0) {
+                                temperature_out() = vals(0);
+                            } else {
+                                Ys_out(i - 1) = vals(i);
+                            }
+                        });
 
-                            member.team_barrier();
-                            density_out() = Impl::RhoMixMs<real_type, DeviceType>::team_invoke(member, temperature_out(), pressure_out(), Ys_out, kmcd_at_i);
-                            member.team_barrier();
-                        } else {
-                            Kokkos::deep_copy(state_out_at_i, state_at_i);
-                        }
+                        member.team_barrier();
+                        density_out() = Impl::RhoMixMs<real_type, DeviceType>::team_invoke(member, temperature_out(), pressure_out(), Ys_out, kmcd_at_i);
+                        member.team_barrier();
+                    } else {
+                        Kokkos::deep_copy(state_out_at_i, state_at_i);
                     }
                 }
-            });
-        Kokkos::Profiling::popRegion();
-    }
+            }
+        });
+    Kokkos::Profiling::popRegion();
+}
 
+struct IgnitionZeroDTemperatureThreshold {
    public:
     /// tadv - an input structure for time marching
     /// state (nSpec+3) - initial condition of the state vector
