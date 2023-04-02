422ab34bf1061ddc8467affe48f0432bcdab9324
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-02 09:41:37 -0400

    debug

--- src/eos/tChemSoot/densityFcn.hpp
+++ src/eos/tChemSoot/densityFcn.hpp
@@ -21,21 +21,21 @@ struct densityFcn {
                                                          /// input
                                                          StateVectorSoot<real_type_1d_view_type> totalState, const KineticModelConstDataType& kmcd) {
         member.team_barrier();
-//        auto gaseousState = real_type_1d_view_type("Gaseous", ::TChem::Impl::getStateVectorSize(kmcd.nSpec));
-//        ::TChem::Impl::StateVector svGas = ::TChem::Impl::StateVector(kmcd.nSpec, gaseousState);
-//
-//        totalState.SplitYiState(svGas);
-//
-//        value_type totalDensity;
-//        auto YCarbon = totalState.MassFractionCarbon();
-//        TCHEM_CHECK_ERROR(!svGas.isValid(), "Error: input state vector is not valid");
-//        {
-//            value_type temperature = svGas.Temperature();
-//            value_type gaseousDensity = Impl::RhoMixMs<value_type, DeviceType>::team_invoke(member, temperature, svGas.Pressure(), svGas.MassFractions(), kmcd);
-//            totalDensity = 1.0 / ((1.0 - YCarbon) / gaseousDensity + (YCarbon / ablate::eos::tChemSoot::solidCarbonDensity));
-//        }
+        auto gaseousState = real_type_1d_view_type("Gaseous", ::TChem::Impl::getStateVectorSize(kmcd.nSpec));
+        ::TChem::Impl::StateVector svGas = ::TChem::Impl::StateVector(kmcd.nSpec, gaseousState);
+
+        totalState.SplitYiState(svGas);
+
+        value_type totalDensity;
+        auto YCarbon = totalState.MassFractionCarbon();
+        TCHEM_CHECK_ERROR(!svGas.isValid(), "Error: input state vector is not valid");
+        {
+            value_type temperature = svGas.Temperature();
+            value_type gaseousDensity = Impl::RhoMixMs<value_type, DeviceType>::team_invoke(member, temperature, svGas.Pressure(), svGas.MassFractions(), kmcd);
+            totalDensity = 1.0 / ((1.0 - YCarbon) / gaseousDensity + (YCarbon / ablate::eos::tChemSoot::solidCarbonDensity));
+        }
+        return totalDensity;
         return 0.0;
-//        return totalDensity;
     }
 };
 
