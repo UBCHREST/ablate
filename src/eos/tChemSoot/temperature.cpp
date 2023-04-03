bfd1311bd51cd3f94b7fdfb59dbdc41ae03cf81f
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-03 13:50:41 -0400

    format and bug fix

--- src/eos/tChemSoot/temperature.cpp
+++ src/eos/tChemSoot/temperature.cpp
@@ -1,7 +1,8 @@
 #include <Kokkos_Core.hpp>
 #ifndef KOKKOS_ENABLE_CUDA
-#include "eos/tChemSoot/temperatureFcn.hpp"
 #include "temperature.hpp"
+
+#include "eos/tChemSoot/temperatureFcn.hpp"
 
 namespace ablate::eos::tChemSoot::impl {
 template <typename PolicyType, typename DeviceType>
