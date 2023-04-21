1a54649773ef2a47dba75ade37217623af8e8338
owenmylotte <owenmylotte@gmail.com>
2023-04-21 10:09:26 -0400

    Rebase and version number.

--- src/radiation/radiation.hpp
+++ src/radiation/radiation.hpp
@@ -4,7 +4,6 @@
 #include <memory>
 #include <set>
 #include "eos/radiationProperties/radiationProperties.hpp"
-//#include "eos/radiationProperties/sootSpectrumProperties.hpp"
 #include "finiteVolume/finiteVolumeSolver.hpp"
 #include "io/interval/interval.hpp"
 #include "monitors/logs/log.hpp"
