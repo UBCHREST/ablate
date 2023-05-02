4bcd95e01f80289048740aad4d60a2dcbcdf1781
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-05-02 16:59:43 -0400

    added check in clang-format

--- config/clangFormatter.cmake
+++ config/clangFormatter.cmake
@@ -10,16 +10,37 @@ FetchContent_MakeAvailable(run-clang-format)
 # Add a target to allow linting check
 find_program(CLANG_FORMAT "clang-format")
 find_package (Python)
-if(CLANG_FORMAT AND Python_Interpreter_FOUND)
-    add_custom_target(
-            format-check
-            COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
-            --style=file
-            -r
-            --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
-            ${PROJECT_SOURCE_DIR}/src
-            ${PROJECT_SOURCE_DIR}/tests
-            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-            USES_TERMINAL
-    )
-endif()
+
+# determine the version
+EXECUTE_PROCESS(COMMAND ${CLANG_FORMAT} --version OUTPUT_VARIABLE CLANG_FORMAT_INFO)
+
+# Special case for version 16
+if(${CLANG_FORMAT_INFO} MATCHES " 16.")
+    if(CLANG_FORMAT AND Python_Interpreter_FOUND)
+        add_custom_target(
+                format-check
+                COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
+                --style=file:config/.clang-format.v16
+                -r
+                --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
+                ${PROJECT_SOURCE_DIR}/src
+                ${PROJECT_SOURCE_DIR}/tests
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                USES_TERMINAL
+        )
+    endif()
+elseif ()
+    if(CLANG_FORMAT AND Python_Interpreter_FOUND)
+        add_custom_target(
+                format-check
+                COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
+                --style=file
+                -r
+                --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
+                ${PROJECT_SOURCE_DIR}/src
+                ${PROJECT_SOURCE_DIR}/tests
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                USES_TERMINAL
+        )
+    endif()
+endif ()
