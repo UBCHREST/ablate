4c79cf899bc678dcae56d9930992b97af2400d77
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-05-03 09:16:20 -0400

    simplified check

--- config/clangFormatter.cmake
+++ config/clangFormatter.cmake
@@ -9,38 +9,31 @@ FetchContent_MakeAvailable(run-clang-format)
 
 # Add a target to allow linting check
 find_program(CLANG_FORMAT "clang-format")
-find_package (Python)
+find_package(Python)
 
 # determine the version
 EXECUTE_PROCESS(COMMAND ${CLANG_FORMAT} --version OUTPUT_VARIABLE CLANG_FORMAT_INFO)
+STRING(REGEX MATCH "([0-9]+).([0-9]+).([0-9]+)" CLANG_FORMAT_VERSION ${CLANG_FORMAT_INFO})
 
-# Special case for version 16
-if(${CLANG_FORMAT_INFO} MATCHES " 16.")
-    if(CLANG_FORMAT AND Python_Interpreter_FOUND)
-        add_custom_target(
-                format-check
-                COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
-                --style=file:config/.clang-format.v16
-                -r
-                --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
-                ${PROJECT_SOURCE_DIR}/src
-                ${PROJECT_SOURCE_DIR}/tests
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                USES_TERMINAL
-        )
-    endif()
-elseif ()
-    if(CLANG_FORMAT AND Python_Interpreter_FOUND)
-        add_custom_target(
-                format-check
-                COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
-                --style=file
-                -r
-                --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
-                ${PROJECT_SOURCE_DIR}/src
-                ${PROJECT_SOURCE_DIR}/tests
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                USES_TERMINAL
-        )
-    endif()
+# Set the style file based upon the file, first the default
+SET(CLANG_FORMAT_STYLE "--style=file")
+
+# Special case for versions less than 17
+if (${CLANG_FORMAT_VERSION} LESS 17)
+    SET(CLANG_FORMAT_STYLE "--style=file:config/.clang-format.v16")
+endif ()
+
+# Special case for versions less than 17
+if (CLANG_FORMAT AND Python_Interpreter_FOUND)
+    add_custom_target(
+            format-check
+            COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
+            ${CLANG_FORMAT_STYLE}
+            -r
+            --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
+            ${PROJECT_SOURCE_DIR}/src
+            ${PROJECT_SOURCE_DIR}/tests
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            USES_TERMINAL
+    )
 endif ()
