fa9b79d82db8df95e7045c5a64095b0972a7f3bf
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-05-10 12:56:07 -0400

    moved clang-format code inside check

--- config/clangFormatter.cmake
+++ config/clangFormatter.cmake
@@ -11,20 +11,20 @@ FetchContent_MakeAvailable(run-clang-format)
 find_program(CLANG_FORMAT "clang-format")
 find_package(Python)
 
-# determine the version
-EXECUTE_PROCESS(COMMAND ${CLANG_FORMAT} --version OUTPUT_VARIABLE CLANG_FORMAT_INFO)
-STRING(REGEX MATCH "([0-9]+).([0-9]+).([0-9]+)" CLANG_FORMAT_VERSION ${CLANG_FORMAT_INFO})
-
-# Set the style file based upon the file, first the default
-SET(CLANG_FORMAT_STYLE "--style=file")
-
-# Special case for versions less than 17
-if (${CLANG_FORMAT_VERSION} LESS 17)
-    SET(CLANG_FORMAT_STYLE "--style=file:config/.clang-format.v16")
-endif ()
-
 # Special case for versions less than 17
 if (CLANG_FORMAT AND Python_Interpreter_FOUND)
+    # determine the version
+    EXECUTE_PROCESS(COMMAND ${CLANG_FORMAT} --version OUTPUT_VARIABLE CLANG_FORMAT_INFO)
+    STRING(REGEX MATCH "([0-9]+).([0-9]+).([0-9]+)" CLANG_FORMAT_VERSION ${CLANG_FORMAT_INFO})
+
+    # Set the style file based upon the file, first the default
+    SET(CLANG_FORMAT_STYLE "--style=file")
+
+    # Special case for versions less than 17
+    if (${CLANG_FORMAT_VERSION} LESS 17)
+        SET(CLANG_FORMAT_STYLE "--style=file:config/.clang-format.v16")
+    endif ()
+
     add_custom_target(
             format-check
             COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
