54fbfb3db4cb6e7b4b1396f7d3d323a1b99130fc
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-05-19 09:04:07 -0400

    removed test code

--- src/domain/subDomain.cpp
+++ src/domain/subDomain.cpp
@@ -28,13 +28,6 @@ ablate::domain::SubDomain::SubDomain(Domain& domainIn, PetscInt dsNumber, const 
         name = std::string(labelName);
         labelValue = 1;  // assume that the region value is one for now until a different value is returned from DMGetRegionNumDS
     }
-    std::vector<double> test;
-    for (int i = 0; i < 10; ++i) {
-        test.push_back(i);
-    }
-
-    double sum = test[200];
-    setenv("blue", std::to_string(sum).c_str(), true);
 
     // Get a reference to local fields
     if (fieldMap) {
