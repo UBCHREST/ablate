e13cfa6faac9f67fa7a9168f857cd7dd32c7ec2c
maesemen <maesemen@buffalo.edu>
2023-06-01 08:53:20 -0400

    surface force using density, still need to calc grad_rho

--- src/finiteVolume/processes/surfaceForce.cpp
+++ src/finiteVolume/processes/surfaceForce.cpp
@@ -3,6 +3,7 @@
 #include "registrar.hpp"
 #include "utilities/constants.hpp"
 #include "utilities/mathUtilities.hpp"
+#include "boundarySolver/boundarySolver.hpp"
 
 ablate::finiteVolume::processes::SurfaceForce::SurfaceForce(PetscReal sigma) : sigma(sigma) {}
 
@@ -272,9 +273,12 @@ PetscErrorCode ablate::finiteVolume::processes::SurfaceForce::ComputeSource(cons
         // calculate curvature -->  kappa = 1/n [(n/|n|. Delta) |n| - (Delta.n)]
         curvature = (totalGradMagNormal - totalDivNormal) / (magCellNormal + utilities::Constants::tiny);
 
+        PetscReal grad_rho[3];
+        //boundarySolver::BoundarySolver::ComputeGradient(dim, boundaryValues[uOff[density]], stencilSize, &pointValues[0], stencilWeights, source + sOff[sourceField] + (0 * dim));
+
         for (PetscInt d = 0; d < dim; ++d) {
             // calculate surface force and energy
-            surfaceForce[d] = process->sigma * curvature * cellCenterNormal[d];
+            surfaceForce[d] = process->sigma * curvature * grad_rho[d]/(994.0897506154375-1.1614401858304297) * density/(0.5*(994.0897506154375+1.1614401858304297));//process->sigma * curvature * cellCenterNormal[d];
             surfaceEnergy += surfaceForce[d] * vel[d];
             // add in the contributions
             eulerSource[ablate::finiteVolume::CompressibleFlowFields::RHOU + d] = surfaceForce[d];
