51cb4ffd7e4831d119b41b247d0838611aad10c0
David Salac <davidsal@buffalo.edu>
2023-07-24 09:36:24 -0400

    Readability

--- src/mathFunctions/fieldFunction.cpp
+++ src/mathFunctions/fieldFunction.cpp
@@ -8,6 +8,7 @@ ablate::mathFunctions::FieldFunction::FieldFunction(std::string fieldName, std::
 
 #include "registrar.hpp"
 REGISTER_DEFAULT(ablate::mathFunctions::FieldFunction, ablate::mathFunctions::FieldFunction, "a field description that can be used for initialization or exact solution ",
-                 ARG(std::string, "fieldName", "the field name"), ARG(ablate::mathFunctions::MathFunction, "field", "the math function used to describe the field"),
+                 ARG(std::string, "fieldName", "the field name"),
+                 ARG(ablate::mathFunctions::MathFunction, "field", "the math function used to describe the field"),
                  OPT(ablate::mathFunctions::MathFunction, "timeDerivative", "the math function used to describe the field time derivative"),
                  OPT(ablate::domain::Region, "region", "A subset of the domain to apply the field function"));
