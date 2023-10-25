ce369dd1d07e0f10fe1ee1748de8b49f97479ea2
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-10-25 13:40:36 -0400

    working on multi-mesh type support

--- config/findXdmfGenerator.cmake
+++ config/findXdmfGenerator.cmake
@@ -12,8 +12,8 @@ ELSE ()
     set(DISABLE_XDMFGENERATOR_TESTS ON CACHE BOOL "" FORCE)
     FetchContent_Declare(
             xdmfGeneratorLibrary
-            GIT_REPOSITORY https://github.com/UBCHREST/XdmfGenerator.git
-            GIT_TAG v0.1.15
+            GIT_REPOSITORY https://github.com/mmcgurn/petscXdmfGenerator.git
+            GIT_TAG mcgurn/default-stream
     )
     FetchContent_MakeAvailable(xdmfGeneratorLibrary)
     # Put the library into CHREST namespace
