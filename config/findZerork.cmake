58d92a2a9b67cc0e166d4fbeb775941c7db0ae44
kolosret <kolosret@buffalo.edu>
2024-04-11 16:00:22 -0700

    combining all build options for zerork into one cmake file

--- config/findZerork.cmake
+++ config/findZerork.cmake
@@ -1,17 +1,22 @@
 
 # check to see if the ZERORK_DIR was specified, if not build
-if (NOT (DEFINED ENV{ZERORK_DIR}))
+if (NOT (DEFINED ENV{ZERORK_DIRz}))
     message(STATUS "ZERORK_DIR not set.  Downloading and building zerork...")
 
-    FetchContent_Declare(zerork
+
+    # CPU build
+    if (NOT (DEFINED ENV{ABLATE_GPU}))
+        message(STATUS "Builing zerork for CPUs.")
+
+        FetchContent_Declare(zerork
             GIT_REPOSITORY https://github.com/LLNL/zero-rk.git
-            GIT_TAG 71befd3f6ff6f49a66e07b82659b88d2bc1beb8c
+            GIT_TAG 71befd3f6ff6f49a66e07b82659b88d2bc1beb8c  #git main branch for both cpu and CUDA
             )
-    FetchContent_MakeAvailable(zerork)
+        FetchContent_MakeAvailable(zerork)
 
-    set_include_directories_as_system(zerork)
+        set_include_directories_as_system(zerork)
 
-    install(TARGETS zerork_cfd_plugin zerork_vectormath ckconverter zerorkutilities zerork spify
+        install(TARGETS zerork_cfd_plugin zerork_vectormath ckconverter zerorkutilities zerork spify
             EXPORT ablateTargets
             LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
             ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
@@ -19,6 +24,52 @@ if (NOT (DEFINED ENV{ZERORK_DIR}))
             INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
             )
 
+        add_library(ZERORK::zerork_cfd_plugin ALIAS zerork_cfd_plugin)
+
+    #Nvidia build
+    elseif ($ENV{ABLATE_GPU} STREQUAL "CUDA")
+        message(STATUS "Builing zerork for Nvidia GPUs with cuda.")
+
+        FetchContent_Declare(zerork
+                GIT_REPOSITORY https://github.com/LLNL/zero-rk.git
+                GIT_TAG 71befd3f6ff6f49a66e07b82659b88d2bc1beb8c  #git main branch for both cpu and CUDA
+        )
+        FetchContent_MakeAvailable(zerork)
+
+        set_include_directories_as_system(zerork)
+
+        install(TARGETS zerork_cfd_plugin_gpu zerork_cfd_plugin ckconverter zerork_vectormath zerorkutilities zerork zerork_cuda spify
+            EXPORT ablateTargets
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+            INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+        )
+
+        add_library(ZERORK::zerork_cfd_plugin ALIAS zerork_cfd_plugin_gpu)
+
+    elseif($ENV{ABLATE_GPU} STREQUAL "ROCM")
+        message(STATUS "Builing zerork for AMD GPUs and hip.")
+
+        FetchContent_Declare(zerork
+            GIT_REPOSITORY https://github.com/LLNL/zero-rk.git
+            GIT_TAG af50489b8ac6a24b025cf49f6e16399bdb5a8342  #points to zerork hip branch
+            )
+        FetchContent_MakeAvailable(zerork)
+
+        set_include_directories_as_system(zerork)
+
+        install(TARGETS zerork_cfd_plugin_gpu zerork_cfd_plugin ckconverter zerork_vectormath zerorkutilities zerork zerork_cuda spify
+            EXPORT ablateTargets
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+            INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+            )
+
+        add_library(ZERORK::zerork_cfd_plugin ALIAS zerork_cfd_plugin_gpu)
+
+    endif()
 
 elseif (DEFINED ENV{ZERORK_DIR})
     message(STATUS "Found ZERORK_DIR, using prebuilt zerork")
@@ -27,6 +78,8 @@ elseif (DEFINED ENV{ZERORK_DIR})
     add_library(zerork_cfd_plugin INTERFACE IMPORTED GLOBAL)
     target_include_directories(zerork_cfd_plugin INTERFACE "$ENV{ZERORK_DIR}/include")
     target_link_libraries(zerork_cfd_plugin INTERFACE "$ENV{ZERORK_DIR}/lib/libzerork_cfd_plugin.so")
+
+    add_library(ZERORK::zerork_cfd_plugin ALIAS zerork_cfd_plugin)
 
 endif ()
 
