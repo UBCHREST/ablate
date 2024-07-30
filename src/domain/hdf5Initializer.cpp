0102e9e8162ebed6afa97d4a5fe3a556c34e656a
klbudzin <klbudzin@buffalo.edu>
2024-07-30 14:53:49 -0400

    Fixing Petsc API Change

--- src/domain/hdf5Initializer.cpp
+++ src/domain/hdf5Initializer.cpp
@@ -43,7 +43,7 @@ ablate::domain::Hdf5Initializer::Hdf5Mesh::Hdf5Mesh(const std::filesystem::path&
 }
 ablate::domain::Hdf5Initializer::Hdf5Mesh::~Hdf5Mesh() {
     // close the viewer
-    PetscOptionsRestoreViewer(&petscViewer) >> utilities::PetscUtilities::checkError;
+    PetscViewerDestroy(&petscViewer) >> utilities::PetscUtilities::checkError;
 
     // free the memory with the mesh
     DMDestroy(&dm) >> utilities::PetscUtilities::checkError;
--- tests/testingResources/petscTestViewer.cpp
+++ tests/testingResources/petscTestViewer.cpp
@@ -17,7 +17,7 @@ testingResources::PetscTestViewer::~PetscTestViewer() {
     file = NULL;
 
     // and destroy the viewer
-    PetscOptionsRestoreViewer(&viewer);
+    PetscViewerDestroy(&viewer);
 }
 std::string testingResources::PetscTestViewer::GetString() {
     // build a string stream to hold the file
